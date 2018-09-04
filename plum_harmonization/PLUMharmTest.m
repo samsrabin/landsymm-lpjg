8af239c79c3a1b0354e9585892c5ef8942be513d
Sam Rabin <sam.rabin@kit.edu>
2018-09-04 12:58:12 +0200

    In PLUMharmTest.m, can now read management inputs and make time series of global Nfert applications.

--- plum_harmonization/PLUMharmTest.m
+++ plum_harmonization/PLUMharmTest.m
@@ -6,15 +6,19 @@ thisVer = '' ;
 % thisVer = 'v2.' ;
 
 runList = {'SSP1.v10.s1' ;
-            'SSP3.v10.s1' ;
-            'SSP4.v10.s1' ;
-            'SSP5.v10.s1'} ;
+%             'SSP3.v10.s1' ;
+%             'SSP4.v10.s1' ;
+%             'SSP5.v10.s1';
+            } ;
 
 base_year = 2010 ;
 
-yearList = 2011:2100 ;
+yearList = 2011:2050 ;
 
 norm2extra = 0.177 ;
+
+% Method for inpaint_nans()
+inpaint_method = 0 ;
 
 out_dir = '/Users/sam/Documents/Dropbox/LPJ-GUESS-PLUM/LPJGP_paper02_Sam/harmonization_figs_20180831/' ;
 
@@ -36,10 +40,14 @@ addpath(genpath('/Users/sam/Documents/Dropbox/LPJ-GUESS-PLUM/LPJGP_paper02_Sam/M
 % lons_map = repmat(-180:0.5:179.5,[360 1]) ;
 % lats_map = repmat((-90:0.5:89.5)',[1 720]) ;
 
+% Conversion factors
+cf_kg2Mt = 1e-3*1e-6 ;
+cf_m2_to_km2 = (1e-3)^2 ;
 
-%% Import and aggregate reference LU map
 
-disp('Importing reference LU map...')
+%% Import reference data
+
+disp('Importing reference data...')
 
 % Get LUH2 land area (km2)
 landarea_file = '/Users/Shared/PLUM/crop_calib_data/other/staticData_quarterdeg.nc' ;
@@ -51,7 +59,7 @@ tmp = landArea_YXqd(:,1:2:1440) + landArea_YXqd(:,2:2:1440) ;
 landArea_YX = tmp(1:2:720,:) + tmp(2:2:720,:) ;
 clear *_YXqd
 
-% Import LUH2
+% Import LUH2 base_year
 luh2_file = '/Users/Shared/PLUM/input/LU/lu_1850_2015_luh2_aggregate_sum2x2_midpoint_nourban_orig_v21.txt' ;
 luh2 = lpjgu_matlab_readTable_then2map(luh2_file) ;
 if ~isempty(find(luh2.maps_YXvy(:,:,contains(luh2.varNames,{'URBAN','PEATLAND'}),:)>0,1))
@@ -104,6 +112,24 @@ Ncrops_lpjg = length(LPJGcrops) ;
 Ncrops = Ncrops_lpjg ;
 Nagri = Ncrops_lpjg + 1 ;
 clear tmp
+% Get "irrigation intensity" as fraction of thisCrop that is irrigated
+% (later, will interpolate to cells without any of thisCrop)
+luh2_irrig.varNames = LPJGcrops ;
+luh2_irrig.maps_YXv = nan([size(landArea_YX) Ncrops_lpjg],'single') ;
+for c = 1:Ncrops_lpjg
+    thisCrop = LPJGcrops{c} ;
+    if strcmp(thisCrop,'ExtraCrop')
+        luh2_irrig.maps_YXv(:,:,c) = zeros(size(landArea_YX)) ;
+        continue
+    end
+    thisCropI = [thisCrop 'i'] ;
+    thisCropR_YX = luh2_cropf.maps_YXv(:,:,strcmp(luh2_cropf.varNames,thisCrop)) ;
+    thisCropI_YX = luh2_cropf.maps_YXv(:,:,strcmp(luh2_cropf.varNames,thisCropI)) ;
+    thisCrop_YX = thisCropR_YX + thisCropI_YX ;
+    tmp_YX = thisCropI_YX ./ thisCrop_YX ;
+    tmp_YX(thisCrop_YX==0) = NaN ;
+    luh2_irrig.maps_YXv(:,:,c) = tmp_YX ;
+end
 % Combine irrigated and rainfed
 any_irrigated = ~isequal(luh2_cropf.varNames,LPJGcrops) ;
 if any_irrigated
@@ -127,6 +153,29 @@ if any_irrigated
     clear tmp
 end
 
+% Read baseline fertilization
+% (later, will interpolate to cells without any of thisCrop)
+luh2_nfert = lpjgu_matlab_readTable_then2map('/Users/Shared/PLUM/input/remaps_v4/nfert.remapv4.20180214.cgFertIrr0.setaside0103.m0.txt',...
+    'verboseIfNoMat',false,'force_mat_save',true) ;
+% Get just base year, if needed
+if isfield(luh2_nfert,'yearList')
+    tmp = luh2_nfert.maps_YXvy(:,:,luh2_nfert.yearList==base_year) ;
+    luh2_nfert = rmfield(luh2_nfert,{'maps_YXvy','yearList'}) ;
+    luh2_nfert.maps_YXv = tmp ;
+    clear tmp
+end
+% Add ExtraCrop, if needed
+if any(strcmp(LPJGcrops,'ExtraCrop')) && ~any(strcmp(luh2_nfert.varNames,'ExtraCrop'))
+    luh2_nfert.varNames = [luh2_nfert.varNames {'ExtraCrop'}] ;
+    luh2_nfert.maps_YXv = cat(3,luh2_nfert.maps_YXv,zeros(size(landArea_YX))) ;
+end
+[~,IA,IB] = intersect(LPJGcrops,luh2_nfert.varNames,'stable') ;
+if ~isequal(IA',1:Ncrops_lpjg)
+    error('Issue with crop compatibility between LPJGcrops and baseline Nfert.')
+end
+luh2_nfert.maps_YXv = luh2_nfert.maps_YXv(:,:,IB) ;
+luh2_nfert.varNames = LPJGcrops ;
+
 % Convert luh2 to have individual crops instead of CROPLAND
 if ~exist('luh2_orig','var')
     luh2_orig = luh2 ;
@@ -146,23 +195,16 @@ LUnames_agri = LUnames(isAgri) ;
 isAgri_isPast = strcmp(LUnames_agri,'PASTURE') ;
 isCrop = ~strcmp(LUnames,'NATURAL') & ~strcmp(LUnames,'PASTURE') & notBare ;
 
-% Get time series
+% Convert NaNs to zeros for addition
 luh2.maps_YXvy(isnan(luh2.maps_YXvy)) = 0 ;
-ts_luh2_cy = squeeze(sum(sum(luh2.maps_YXvy(:,:,isCrop,:),2),1)) ;
+luh2_nfert.maps_YXv(isnan(luh2_nfert.maps_YXv)) = 0 ;
+luh2_irrig.maps_YXv(isnan(luh2_irrig.maps_YXv)) = 0 ;
 
 % Get LUH2 fraction that is vegetated, barren
 luh2_vegd_YX = sum(luh2.maps_YXvy(:,:,notBare,end),3) ;
 luh2_vegdFrac_YX = luh2_vegd_YX ./ landArea_YX ;
 luh2_bare_YX = luh2.maps_YXvy(:,:,strcmp(LUnames,'BARREN'),end) ;
 luh2_bareFrac_YX = luh2_bare_YX ./ landArea_YX ;
-             
-% % Get lats/lons
-% list2map_2deg = find(landArea_2deg_YX>0) ;
-% lons_2deg = lons_map_2deg(list2map_2deg) ;
-% lats_2deg = lats_map_2deg(list2map_2deg) ;
-% list2map = find(landArea_YX>0) ;
-% lons = lons_map(list2map) ;
-% lats = lats_map(list2map) ;
 
 % Get PLUM crop types
 file_in = [PLUM_base_in 'LandUse.txt'] ;
@@ -192,7 +234,7 @@ PLUMtoLPJG{strcmp(LPJGcrops,'ExtraCrop')} = 'setaside' ;
 % Check that every PLUM crop is mapped
 checkPLUMmap(PLUMtoLPJG,PLUMcrops) ;
 
-disp('Done importing reference LU map and doing other setup.')
+disp('Done importing reference data.')
 
 
 
@@ -203,26 +245,36 @@ bareFrac_y0_YX = luh2_bareFrac_YX ;
 
 disp('Setting up PLUM*_YXvyr arrays...')
 PLUMorig_YXvyr = nan(360,720,Nlu,Nyears,Nruns,'single') ;
+PLUMorig_nfert_YXvyr = nan(360,720,Ncrops,Nyears,Nruns,'single') ;
+PLUMorig_irrig_YXvyr = nan(360,720,Ncrops,Nyears,Nruns,'single') ;
 PLUMharm_YXvyr = nan(360,720,Nlu,Nyears,Nruns,'single') ;
+PLUMharm_nfert_YXvyr = nan(360,720,Ncrops,Nyears,Nruns,'single') ;
+PLUMharm_irrig_YXvyr = nan(360,720,Ncrops,Nyears,Nruns,'single') ;
 
 for r = 1:Nruns
     thisRun = removeslashifneeded(runList{r}) ;
     
     % Original
     fprintf('Importing %s...\n', thisRun) ;
-    S_out = PLUMharm_pp_readPLUM([topDir thisRun], base_year, yearList, ...
+    [S_out, S_nfert_out, S_irrig_out] = PLUMharm_pp_readPLUM(...
+        [topDir thisRun], base_year, yearList, ...
         landArea_YX, LUnames, PLUMtoLPJG, LPJGcrops, ...
-        is2deg, bareFrac_y0_YX, norm2extra) ;
-    PLUMorig_YXvyr(:,:,:,:,r) = S_out.maps_YXvy ;
-    clear S_out
+        is2deg, bareFrac_y0_YX, norm2extra, inpaint_method) ;
+    PLUMorig_YXvyr(:,:,:,:,r) = S_out.maps_YXvy(:,:,:,2011:2100<=max(yearList)) ;
+    PLUMorig_nfert_YXvyr(:,:,:,:,r) = S_nfert_out.maps_YXvy(:,:,:,2011:2100<=max(yearList)) ;
+    PLUMorig_irrig_YXvyr(:,:,:,:,r) = S_irrig_out.maps_YXvy(:,:,:,2011:2100<=max(yearList)) ;
+    clear S_*out
     
     % Harmonized
     fprintf('Importing %s.harm...\n', thisRun) ;
-    S_out = PLUMharm_pp_readPLUM([topDir thisRun '.harm'],base_year,yearList, ...
+    [S_out, S_nfert_out, S_irrig_out] = PLUMharm_pp_readPLUM(...
+        [topDir thisRun '.harm'],base_year,yearList, ...
         landArea_YX, LUnames, PLUMtoLPJG, LPJGcrops, ...
-        is2deg, [], 0) ;
+        is2deg, [], 0, []) ;
     PLUMharm_YXvyr(:,:,:,:,r) = S_out.maps_YXvy ;
-    clear S_out
+    PLUMharm_nfert_YXvyr(:,:,:,:,r) = S_nfert_out.maps_YXvy ;
+    PLUMharm_irrig_YXvyr(:,:,:,:,r) = S_irrig_out.maps_YXvy ;
+    clear S_*out
     
 end
 
@@ -293,6 +345,38 @@ end
 
 % Save
 export_fig([out_dir 'timeSeries_crops.pdf']) ;
+
+
+%% Time series of Nfert
+
+ts_luh2_cy = cf_kg2Mt/cf_m2_to_km2 .* squeeze(nansum(nansum(luh2.maps_YXvy(:,:,isCrop,:) .* repmat(luh2_nfert.maps_YXv,[1 1 1 Nyears_luh2]),1),2)) ;
+ts_orig_cyr = cf_kg2Mt/cf_m2_to_km2 .* squeeze(nansum(nansum(PLUMorig_YXvyr(:,:,isCrop,:,:) .* PLUMorig_nfert_YXvyr,1),2)) ;
+ts_harm_cyr = cf_kg2Mt/cf_m2_to_km2 .* squeeze(nansum(nansum(PLUMharm_YXvyr(:,:,isCrop,:,:) .* PLUMharm_nfert_YXvyr,1),2)) ;
+
+spacing = [0.05 0.1] ;
+
+figure('Color','w','Position',figurePos)
+
+incl_luh2_years = yearList_luh2>=1950 & yearList_luh2<=2010 ;
+
+for v = 1:Ncrops
+    subplot_tight(4,2,v,spacing) ;
+    plot(yearList_luh2(incl_luh2_years),ts_luh2_cy(v,incl_luh2_years),'-k','LineWidth',2) ;
+    set(gca,'ColorOrderIndex',1) ;
+    hold on
+    plot(yearList,squeeze(ts_harm_cyr(v,:,:)),'-','LineWidth',1)
+    set(gca,'ColorOrderIndex',1) ;
+    plot(yearList,squeeze(ts_orig_cyr(v,:,:)),'--','LineWidth',1)
+    hold off
+    title(LPJGcrops{v})
+    set(gca,'FontSize',14)
+    ylabel('Mt')
+    legend({'LUH2','SSP1','SSP3','SSP4','SSP5'},'Location','NorthWest')
+end
+
+% Save
+export_fig([out_dir 'timeSeries_nfert.pdf']) ;
+
 
 
 %% Maps: At three years
--- plum_harmonization/PLUMharm_pp_readPLUM.m
+++ plum_harmonization/PLUMharm_pp_readPLUM.m
@@ -1,8 +1,7 @@
-function S_out = PLUMharm_pp_readPLUM(inDir,base_year,yearList, ...
+function [S_out, S_nfert_out, S_irrig_out] = PLUMharm_pp_readPLUM(...
+    inDir,base_year,yearList, ...
     landArea_YX, LUnames, PLUMtoLPJG, LPJGcrops, ...
-    is2deg, bareFrac_y0_YX, norm2extra)
-
-is_orig = ~isempty(bareFrac_y0_YX) ;
+    is2deg, bareFrac_y0_YX, norm2extra, inpaint_method)
 
 % Does MAT-file already exist?
 MATfile = [inDir '.processed.mat'] ;
@@ -12,27 +11,35 @@ if exist(MATfile,'file')
     TXTfile_info = dir(TXTfile) ;
     if MATfile_info.datenum > TXTfile_info.datenum
         S_out = [] ;
-        disp('Loading MAT file...')
-        load(MATfile,'S_out') ;
+        S_nfert_out = [] ;
+        S_irrig_out = [] ;
+        disp('   Loading MAT file...')
+        load(MATfile,'S_out','S_nfert_out','S_irrig_out') ;
     else
         warning('.mat file exists but is older than latest .txt file. Regenerating.')
-        S_out = generate_struct(inDir,base_year,yearList, ...
-            landArea_YX, LUnames, PLUMtoLPJG, LPJGcrops, ...
-            is2deg, bareFrac_y0_YX, norm2extra) ;
-        disp('Saving MAT file...')
+        [S_out, S_nfert_out, S_irrig_out] = ...
+            generate_struct( ...
+                inDir,base_year,yearList, ...
+                landArea_YX, LUnames, PLUMtoLPJG, LPJGcrops, ...
+                is2deg, bareFrac_y0_YX, norm2extra, inpaint_method) ;
+        % Save, catching exceptions to avoid having to re-read everything
+        disp('   Saving MAT file...')
         try
-            save(MATfile,'S_out') ;
+            save(MATfile,'S_out', 'S_nfert_out', 'S_irrig_out') ;
         catch ME
             keyboard
         end
     end
 else
-    S_out = generate_struct(inDir,base_year,yearList, ...
-        landArea_YX, LUnames, PLUMtoLPJG, LPJGcrops, ...
-        is2deg, bareFrac_y0_YX, norm2extra) ;
-    disp('Saving MAT file...')
+    [S_out, S_nfert_out, S_irrig_out] = ...
+        generate_struct( ...
+            inDir,base_year,yearList, ...
+            landArea_YX, LUnames, PLUMtoLPJG, LPJGcrops, ...
+            is2deg, bareFrac_y0_YX, norm2extra, inpaint_method) ;
+    % Save, catching exceptions to avoid having to re-read everything
+    disp('   Saving MAT file...')
     try
-        save(MATfile,'S_out') ;
+        save(MATfile,'S_out', 'S_nfert_out', 'S_irrig_out') ;
     catch ME
         keyboard
     end
@@ -45,14 +52,18 @@ end
 
 
 
-function S = generate_struct(inDir, base_year, yearList, ...
+function [S, S_nfert, S_irrig] = generate_struct(...
+    inDir, base_year, yearList, ...
     landArea_YX, LUnames, PLUMtoLPJG, LPJGcrops, ...
-    is2deg, bareFrac_y0_YX, norm2extra)
+    is2deg, bareFrac_y0_YX, norm2extra, inpaint_method)
 
 S.varNames = LUnames ;
+S_nfert.varNames = LPJGcrops ;
+S_irrig.varNames = LPJGcrops ;
 Nyears = length(yearList) ;
 
 is_orig = ~isempty(bareFrac_y0_YX) ;
+landArea_2deg_YX = PLUMharm_aggregate(landArea_YX,0.5,2) ;
 
 for y = 1:Nyears
     
@@ -74,20 +85,28 @@ for y = 1:Nyears
             fprintf('%d... ',thisYear)
         end
         if is2deg
-            [~,PLUM_in] = PLUMharm_processPLUMin_areaCrops(...
-                file_in,landArea_YX, LUnames, bareFrac_y0_YX, ...
-                PLUMtoLPJG, LPJGcrops, norm2extra) ;
+            [~, ~, ~, PLUM_in, nfert_in, irrig_in] = PLUMharm_processPLUMin_areaCrops(...
+                file_in,landArea_YX, landArea_2deg_YX, LUnames, bareFrac_y0_YX, ...
+                [], [], PLUMtoLPJG, LPJGcrops, norm2extra, inpaint_method) ;
         else
-            [PLUM_in,~] = PLUMharm_processPLUMin_areaCrops(...
-                file_in,landArea_YX, LUnames, bareFrac_y0_YX, ...
-                PLUMtoLPJG, LPJGcrops, norm2extra) ;
+            [PLUM_in, nfert_in, irrig_in, ~, ~, ~] = PLUMharm_processPLUMin_areaCrops(...
+                file_in,landArea_YX, landArea_2deg_YX, LUnames, bareFrac_y0_YX, ...
+                [], [], PLUMtoLPJG, LPJGcrops, norm2extra, inpaint_method) ;
         end
         if y==1
             S.maps_YXvy = nan(size(PLUM_in.maps_YXv,1), ...
                               size(PLUM_in.maps_YXv,2), ...
                               length(LUnames), Nyears, 'single') ;
+            S_nfert.maps_YXvy = nan(size(nfert_in.maps_YXv,1), ...
+                                    size(nfert_in.maps_YXv,2), ...
+                                    length(LPJGcrops), Nyears, 'single') ;
+            S_irrig.maps_YXvy = nan(size(nfert_in.maps_YXv,1), ...
+                                    size(nfert_in.maps_YXv,2), ...
+                                    length(LPJGcrops), Nyears, 'single') ;
         end
         S.maps_YXvy(:,:,:,y) = PLUM_in.maps_YXv ;
+        S_nfert.maps_YXvy(:,:,:,y) = nfert_in.maps_YXv ;
+        S_irrig.maps_YXvy(:,:,:,y) = irrig_in.maps_YXv ;
     else
         warning('\n%d does not exist! Aborting.\n', thisYear)
         break
