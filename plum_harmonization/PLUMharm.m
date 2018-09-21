997d9119846cbf21a00bbe22ec141df9679541c7
Sam Rabin <sam.rabin@kit.edu>
2018-09-21 13:49:38 +0200

    Now passes singles to lpjgu_matlab_saveTable() in PLUMharm2LPJG.m. Previously resolved #24 but marking it resolved here. Also resolved #1 a while back.

--- plum_harmonization/PLUMharm.m
+++ plum_harmonization/PLUMharm.m
@@ -3,14 +3,14 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 base_year = 2010 ;
-year1 = 2024 ;
-yearN = 2024 ;
+year1 = 2011 ;
+yearN = 2100 ;
 
 % Directory for PLUM outputs
 PLUM_in_toptop = {...
-%                   '/Users/Shared/PLUM/PLUM_outputs_for_LPJG/SSP1.v10.s1' ;
-%                   '/Users/Shared/PLUM/PLUM_outputs_for_LPJG/SSP3.v10.s1' ;
-%                   '/Users/Shared/PLUM/PLUM_outputs_for_LPJG/SSP4.v10.s1' ;
+                  '/Users/Shared/PLUM/PLUM_outputs_for_LPJG/SSP1.v10.s1' ;
+                  '/Users/Shared/PLUM/PLUM_outputs_for_LPJG/SSP3.v10.s1' ;
+                  '/Users/Shared/PLUM/PLUM_outputs_for_LPJG/SSP4.v10.s1' ;
                   '/Users/Shared/PLUM/PLUM_outputs_for_LPJG/SSP5.v10.s1' ;
                   } ;
               
