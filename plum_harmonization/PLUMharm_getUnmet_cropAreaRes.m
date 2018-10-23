57f21734c8bc64019e61d024953b3366aa7917c8
Sam Rabin <sam.rabin@kit.edu>
2018-10-23 14:39:18 +0200

    PLUMharm_getUnmet_cropAreaRes now correctly limits agricultural area to max(existing_agri,unreserved) instead of unreserved.

--- plum_harmonization/PLUMharm_getUnmet_cropAreaRes.m
+++ plum_harmonization/PLUMharm_getUnmet_cropAreaRes.m
@@ -11,13 +11,13 @@ proper_zero_denoms = true ;
 % Misc.
 Nagri = size(mid1_y1_2deg_agri_YXv,3) ;
 
-% Get non-reserved area
+% Get unreserved area
 unresArea_2deg_YX = base_2deg_vegd_YX - resArea_2deg_YX ;
 if ~isempty(find(unresArea_2deg_YX>base_2deg_vegd_YX,1))
     error('How is unreserved area > base vegetated area?')
 end
 max_agri_y1_YX = max(unresArea_2deg_YX,sum_agri_y0_YX) ;
-unresArea_2deg_YXv = repmat(unresArea_2deg_YX,[1 1 Nagri]) ;
+max_agri_y1_YXv = repmat(max_agri_y1_YX,[1 1 Nagri]) ;
 
 % Only use output agri from now on
 mid_y1_2deg_agri_YXv = mid1_y1_2deg_agri_YXv ;
@@ -36,16 +36,17 @@ if do_debug
     mid1_y1_2deg_ntrl_YX = base_2deg_vegd_YX - sum(mid1_y1_2deg_agri_YXv,3) ;
     fprintf('      %s\t%0.4e\n',pad('ntrl_y1_mid:',20),mid1_y1_2deg_ntrl_YX(dbI,dbJ)) ;
     fprintf('      %s\t%0.4e\n',pad('vegd_base:',20),base_2deg_vegd_YX(dbI,dbJ)) ;
+    fprintf('      %s\t%0.4e\n',pad('unreserved:',20),unresArea_2deg_YX(dbI,dbJ)) ;
+    fprintf('      %s\t%0.4e\n',pad('max agri:',20),max_agri_y1_YX(dbI,dbJ)) ;
 end
 
-% Check that no agri LU exceeds non-reserved area. Unmet will need to go into
+% Check that no agri LU exceeds unreserved area. Unmet will need to go into
 % agri LUs of neighboring cells.
 % USING FIX TO MAKE UNMET POSITIVE
-% unmetA_YXv = (mid_y1_2deg_agri_YXv - base_2deg_vegd_YXv) .* (mid_y1_2deg_agri_YXv > base_2deg_vegd_YXv) ;
-unmetA_YXv = (mid_y1_2deg_agri_YXv - unresArea_2deg_YXv) .* (mid_y1_2deg_agri_YXv > unresArea_2deg_YXv) ;
+unmetA_YXv = (mid_y1_2deg_agri_YXv - max_agri_y1_YXv) .* (mid_y1_2deg_agri_YXv > max_agri_y1_YXv) ;
 mid_y1_2deg_agri_YXv = mid_y1_2deg_agri_YXv - unmetA_YXv ;
 if do_debug
-    disp('   After check that no LU exceeds non-reserved area:')
+    disp('   After check that no LU exceeds max agri area:')
     fprintf('      %s\t%0.4e\n',pad('agri_y1_out:',20),sum(mid_y1_2deg_agri_YXv(dbI,dbJ,:),3)) ;
     fprintf('      %s\t%0.4e\n',pad('unmetA:',20),sum(unmetA_YXv(dbI,dbJ,:),3)) ;
     if ~isempty(dbCrop)
@@ -70,11 +71,11 @@ if do_debug
 end
 
 
-% Check that sum(agri_y1) does not exceed non-reserved area, and if it
+% Check that sum(agri_y1) does not exceed max agri. area, and if it
 % does, make sure it does not exceed sum(agri_y0). I.e., make sure that it
 % doesn't encroach MORE onto reserved land. NOTE that this also takes care
 % of situation where ntrl is negative, because that indicates that agri
-% exceeds vegd, which means that agri *definitely* exceeds non-reserved. It
+% exceeds vegd, which means that agri *definitely* exceeds unreserved. It
 % also, of course, takes care of conservation of vegetated area.
 % (unmet is POSITIVE)
 sum_agri_y1_YX = sum(mid_y1_2deg_agri_YXv,3) ;
@@ -84,7 +85,7 @@ unmetD_YXv = zeros(size(mid_y1_2deg_agri_YXv)) ;
 while any(any(~isOK_YX))
     j = j + 1 ;
     if j>50
-        error('Possible infinite loop in "Check that sum(agri_y1) does not exceed non-reserved area."')
+        error('Possible infinite loop in "Check that sum(agri_y1) does not exceed max agri. area."')
     end
     exceedLandR_YX = (sum_agri_y1_YX - max_agri_y1_YX) .* (sum_agri_y1_YX > max_agri_y1_YX) ;
     exceedLandR_YXv = repmat(exceedLandR_YX,[1 1 Nagri]) ;
