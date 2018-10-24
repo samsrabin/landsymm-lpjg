ef221413d79080165f0f3b431611a00b0b2641a4
Sam Rabin <sam.rabin@kit.edu>
2018-10-24 16:56:52 +0200

    Added outputs to debug_global_areas().

--- plum_harmonization/debug_global_areas.m
+++ plum_harmonization/debug_global_areas.m
@@ -1,7 +1,7 @@
 function debug_global_areas(...
     mapsA_YXv, mapsB_YXv, ...
     msg_intro, msgA, msgB, ...
-    LPJGcrops, isAgri, dbCrop, thisYear)
+    LUnames, isCrop, isAgri, dbCrop, thisYear)
 
 msgWidth = 45 ;
 units = 'm2' ;
@@ -11,10 +11,24 @@ disp(msg_intro)
 if ~isempty(dbCrop)
     tmp0 = sum(sum(mapsA_YXv(:,:,dbCrop))) ;
     tmp1 = sum(sum(mapsB_YXv(:,:,dbCrop))) ;
-    fprintf('%s %0.4e\n', pad(sprintf('%s_%d glob %s area (%s):', msgA, thisYear-1, LPJGcrops{dbCrop}, units),msgWidth,'left'), tmp0)
-    fprintf('%s %0.4e\n', pad(sprintf('%s_%d glob %s area (%s):', msgB, thisYear,   LPJGcrops{dbCrop}, units),msgWidth,'left'), tmp1)
+    fprintf('%s %0.4e\n', pad(sprintf('%s_%d glob %s area (%s):', msgA, thisYear-1, LUnames{dbCrop}, units),msgWidth,'left'), tmp0)
+    fprintf('%s %0.4e\n', pad(sprintf('%s_%d glob %s area (%s):', msgB, thisYear,   LUnames{dbCrop}, units),msgWidth,'left'), tmp1)
     fprintf('%s %0.4e\n\n', pad(sprintf('diff (%s):',units),msgWidth,'left'), tmp1-tmp0)
 end
+
+% Cropland 
+tmp0 = sum(sum(sum(mapsA_YXv(:,:,isCrop)))) ;
+tmp1 = sum(sum(sum(mapsB_YXv(:,:,isCrop)))) ;
+fprintf('%s %0.4e\n', pad(sprintf('%s_%d glob crop area (%s):', msgA, thisYear-1, units),msgWidth,'left'), tmp0)
+fprintf('%s %0.4e\n', pad(sprintf('%s_%d glob crop area (%s):', msgB, thisYear,   units),msgWidth,'left'), tmp1)
+fprintf('%s %0.4e\n\n', pad(sprintf('diff (%s):',units),msgWidth,'left'), tmp1-tmp0)
+
+% Pasture 
+tmp0 = sum(sum(sum(mapsA_YXv(:,:,strcmp(LUnames,'PASTURE'))))) ;
+tmp1 = sum(sum(sum(mapsB_YXv(:,:,strcmp(LUnames,'PASTURE'))))) ;
+fprintf('%s %0.4e\n', pad(sprintf('%s_%d glob past area (%s):', msgA, thisYear-1, units),msgWidth,'left'), tmp0)
+fprintf('%s %0.4e\n', pad(sprintf('%s_%d glob past area (%s):', msgB, thisYear,   units),msgWidth,'left'), tmp1)
+fprintf('%s %0.4e\n\n', pad(sprintf('diff (%s):',units),msgWidth,'left'), tmp1-tmp0)
 
 % Agricultural 
 tmp0 = sum(sum(sum(mapsA_YXv(:,:,isAgri)))) ;
@@ -23,20 +37,33 @@ fprintf('%s %0.4e\n', pad(sprintf('%s_%d glob agri area (%s):', msgA, thisYear-1
 fprintf('%s %0.4e\n', pad(sprintf('%s_%d glob agri area (%s):', msgB, thisYear,   units),msgWidth,'left'), tmp1)
 fprintf('%s %0.4e\n\n', pad(sprintf('diff (%s):',units),msgWidth,'left'), tmp1-tmp0)
 
+% Natural 
+tmp0 = sum(sum(sum(mapsA_YXv(:,:,strcmp(LUnames,'NATURAL'))))) ;
+tmp1 = sum(sum(sum(mapsB_YXv(:,:,strcmp(LUnames,'NATURAL'))))) ;
+fprintf('%s %0.4e\n', pad(sprintf('%s_%d glob ntrl area (%s):', msgA, thisYear-1, units),msgWidth,'left'), tmp0)
+fprintf('%s %0.4e\n', pad(sprintf('%s_%d glob ntrl area (%s):', msgB, thisYear,   units),msgWidth,'left'), tmp1)
+fprintf('%s %0.4e\n\n', pad(sprintf('diff (%s):',units),msgWidth,'left'), tmp1-tmp0)
+
+% Vegetated 
+tmp0 = sum(sum(sum(mapsA_YXv(:,:,~strcmp(LUnames,'BARREN'))))) ;
+tmp1 = sum(sum(sum(mapsB_YXv(:,:,~strcmp(LUnames,'BARREN'))))) ;
+fprintf('%s %0.4e\n', pad(sprintf('%s_%d glob vegd area (%s):', msgA, thisYear-1, units),msgWidth,'left'), tmp0)
+fprintf('%s %0.4e\n', pad(sprintf('%s_%d glob vegd area (%s):', msgB, thisYear,   units),msgWidth,'left'), tmp1)
+fprintf('%s %0.4e\n\n', pad(sprintf('diff (%s):',units),msgWidth,'left'), tmp1-tmp0)
+
+% Barren 
+tmp0 = sum(sum(sum(mapsA_YXv(:,:,strcmp(LUnames,'BARREN'))))) ;
+tmp1 = sum(sum(sum(mapsB_YXv(:,:,strcmp(LUnames,'BARREN'))))) ;
+fprintf('%s %0.4e\n', pad(sprintf('%s_%d glob bare area (%s):', msgA, thisYear-1, units),msgWidth,'left'), tmp0)
+fprintf('%s %0.4e\n', pad(sprintf('%s_%d glob bare area (%s):', msgB, thisYear,   units),msgWidth,'left'), tmp1)
+fprintf('%s %0.4e\n\n', pad(sprintf('diff (%s):',units),msgWidth,'left'), tmp1-tmp0)
+
 % All land
 tmp0 = sum(mapsA_YXv(:)) ;
 tmp1 = sum(mapsB_YXv(:)) ;
 fprintf('%s %0.4e\n', pad(sprintf('%s_%d glob land area (%s):', msgA, thisYear-1, units),msgWidth,'left'), tmp0)
 fprintf('%s %0.4e\n', pad(sprintf('%s_%d glob land area (%s):', msgB, thisYear,   units),msgWidth,'left'), tmp1)
 fprintf('%s %0.4e\n\n', pad(sprintf('diff (%s):',units),msgWidth,'left'), tmp1-tmp0)
-
-% % Vegetated
-% tmp = sum(sum(out_y0_2deg_vegd_YX(:,:,1:end-1))) ;
-% fprintf('%s_%d_2deg glob vegd area (%s): %0.4e\n', thisYear-1, units, tmp)
-% out_y1_2deg_agri_YX = sum(out_y1_2deg_agri_YXv,3) ;
-% tmp = max(out_y1_2deg_agri_YX(:) - out_y0_2deg_vegd_YX(:)) ;
-% fprintf('Max agri-vegd area (km2): %0.1f\n', tmp)
-% clear tmp*
 
 
 end
