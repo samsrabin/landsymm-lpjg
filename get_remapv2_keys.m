28104776ff10b4a37db66c349cbdec9c3cda443f
Sam Rabin <sam.rabin@kit.edu>
2019-01-24 12:01:43 +0100

    Updated get_remapv2_keys() to work with remaps 7a and 7b. Also added remap jianyong_v01.

--- get_remapv2_keys.m
+++ get_remapv2_keys.m
@@ -3,7 +3,8 @@ function [THISlist_cropsCombined_out, THISin2out_keyCombined_frac, ...
     out_names, out_keys, out_ignores, out_ignore_types] ...
     = get_remapv2_keys(in_version)
 
-out_names = {'20180105b', '20180206', '20180210', '20180212', '20180214', '20180301ani'} ;
+out_names = {'20180105b', '20180206', '20180210', '20180212', '20180214', '20180301ani', ...
+    'WithFruitVegSugar_a', 'WithFruitVegSugar_b', 'jianyong01'} ;
 if ~any(strcmp(out_names,in_version))
     error(['thisVer ' in_version ' not recognized!'])
 end
@@ -113,6 +114,101 @@ out_ignores{strcmp(out_names,thisOne)} = ...
     {'Sugarcane';'Citrus';'Datepalm';'GrapesVine';
     'Cotton';'Cocoa';'Coffee';'OtherAnnuals';'OtherPerennials';
     'FodderGrasses'} ;
+
+% WithFruitVegSugar_a
+% NOTE: Sugarbeet moved from Starchy Roots to Sugar. Should have never been
+% included in Starchy Roots, probably, especially considering that it is
+% its own crop in the FAO data (i.e., not lumped in with Roots And Tubers
+% Total, which was the only item in the calibration for v16/v18
+% previously).
+thisOne = 'WithFruitVegSugar_a' ;
+list_cropsCombined_out = {'CerealsC3','CerealsC4','Rice','Oilcrops','Pulses','StarchyRoots','DateCitGrape','Sugar'} ;
+getOci = @(x) find(strcmp(list_cropsCombined_out,x)) ;
+in2out_keyCombined_frac{getOci('CerealsC3')}   = {'Wheat','Barley','Rye'} ;
+in2out_keyCombined_frac{getOci('CerealsC4')}   = {'Maize','Millet','Sorghum'} ;
+in2out_keyCombined_frac{getOci('Rice')}        = {'Rice'} ;
+in2out_keyCombined_frac{getOci('Oilcrops')}    = {'Sunflower','Soybeans','GroundnutsPeanuts','RapeseedCanola','Oilpalm'} ;
+in2out_keyCombined_frac{getOci('Pulses')}      = {'Pulses'} ;
+in2out_keyCombined_frac{getOci('StarchyRoots')}= {'Potatoes','Cassava'} ;
+in2out_keyCombined_frac{getOci('DateCitGrape')} = {'Datepalm','Citrus','GrapesVine'} ;
+in2out_keyCombined_frac{getOci('Sugar')}       = {'Sugarbeet','Sugarcane'} ;
+out_keys{strcmp(out_names,thisOne)} = in2out_keyCombined_frac ;
+list_cropsCombined_out_ALL{strcmp(out_names,thisOne)} = list_cropsCombined_out ;
+clear in2out_keyCombined_frac
+out_ignores{strcmp(out_names,thisOne)} = ...
+    {'Cotton';'Cocoa';'Coffee';'FodderGrasses';
+     'OtherAnnuals';'OtherPerennials'} ;
+
+% WithFruitVegSugar_b
+% As WithFruitVegSugar_a, but with Other Annual and Other Perennial included in
+% FruitAndVeg.
+% Other annual:
+%   Almonds; Apples; Apricots; Areca Nuts (Betel); Avocados; Bananas; 
+%   Blueberries; Brazil Nuts; Carobs; Cashew Nuts; Cashewapple; Cherries; 
+%   Chestnuts; Cinnamon (Canella); Cloves; Coconuts; Coir; Cranberries; 
+%   Currants; Figs; Fruit Fresh, other; Fruit Tropical Fresh, other; 
+%   Ginger; Gooseberries; Hazelnuts (Filberts); Hops; Kapok Fiber; 
+%   Kapokseed in Shell; Karite Nuts (Sheanuts); Kiwi Fruit; Kolanuts; 
+%   Mangoes; Mate; Natural Gums; Natural Rubber; 
+%   Nutmeg, Mace and Cardamons; Nuts, other; Olives; Papayas; 
+%   Peaches and Nectarines; Pears; Pepper; Persimmons; Pimento; Pineapples;
+%   Pistachios; Plantains; Plums; Quinces; Raspberries; Sour Cherries; 
+%   Stone Fruit other; Strawberries; Tea; Tung Nuts; Vanilla; Walnuts
+% Other perennial:
+%   Abaca (Manila Hemp); Agave Fibers, other; Anise, Badian and Fennel; 
+%   Artichokes; Asparagus; Beans, Green; Beets for Fodder; Berries, other; 
+%   Broad Beans, Green; Buckwheat; Cabbage for Fodder; Cabbages; 
+%   Canary Seed; Cantaloupes & other Melons; Carrots; Carrots for Fodder; 
+%   Castor Beans; Cauliflower; Cereals, other; Chicory Roots; 
+%   Chillies & Peppers, Green; Cucumbers and Gherkins; Eggplants; 
+%   Fiber Crops, other; Flax Fiber and Tow; Fonio; Forage Products, other; 
+%   Garlic; Green Corn (Maize); Green Oilseeds for Fodder; 
+%   Hemp Fiber and Tow; Hempseed; Jute; Jute-Like Fibers; Legumes, other; 
+%   Lettuce; Linseed; Melonseed; Mixed Grain; Mushrooms; Mustard Seed; 
+%   Oats; Oilseeds, other; Okra; Onions & Shallots, Green; Onions, Dry; 
+%   Peas, Green; Peppermint; Poppy Seed; Pumpkins, Squash, Gourds; 
+%   Pyrethrum, Dried Flowers; Quinoa; Ramie; Roots and Tubers, other; 
+%   Safflower Seed; Sesame Seed; Sisal; Spices, other; Spinach; 
+%   String Beans; Sugar Crops, other; Swedes for Fodder; Sweet Potatoes; 
+%   Taro; Tobacco Leaves; Tomatoes; Triticale; Turnips for Fodder; 
+%   Vegetables & Roots for Fodder; Vegetables Fresh, other; Watermelons; 
+%   Yams; Yautia
+thisOne = 'WithFruitVegSugar_b' ;
+list_cropsCombined_out = {'CerealsC3','CerealsC4','Rice','Oilcrops','Pulses','StarchyRoots','FruitAndVeg','Sugar'} ;
+getOci = @(x) find(strcmp(list_cropsCombined_out,x)) ;
+in2out_keyCombined_frac{getOci('CerealsC3')}   = {'Wheat','Barley','Rye'} ;
+in2out_keyCombined_frac{getOci('CerealsC4')}   = {'Maize','Millet','Sorghum'} ;
+in2out_keyCombined_frac{getOci('Rice')}        = {'Rice'} ;
+in2out_keyCombined_frac{getOci('Oilcrops')}    = {'Sunflower','Soybeans','GroundnutsPeanuts','RapeseedCanola','Oilpalm'} ;
+in2out_keyCombined_frac{getOci('Pulses')}      = {'Pulses'} ;
+in2out_keyCombined_frac{getOci('StarchyRoots')}= {'Potatoes','Cassava'} ;
+in2out_keyCombined_frac{getOci('FruitAndVeg')} = {'Datepalm','Citrus','GrapesVine','OtherAnnuals','OtherPerennials'} ;
+in2out_keyCombined_frac{getOci('Sugar')}       = {'Sugarbeet','Sugarcane'} ;
+out_keys{strcmp(out_names,thisOne)} = in2out_keyCombined_frac ;
+list_cropsCombined_out_ALL{strcmp(out_names,thisOne)} = list_cropsCombined_out ;
+clear in2out_keyCombined_frac
+out_ignores{strcmp(out_names,thisOne)} = ...
+    {'Cotton';'Cocoa';'Coffee';'FodderGrasses'} ;
+
+% Jianyong v01
+thisOne = 'jianyong01' ;
+list_cropsCombined_out = {'Wheat','Maize','Sorghum','Rice','Soybean','FabaBean'} ;
+getOci = @(x) find(strcmp(list_cropsCombined_out,x)) ;
+in2out_keyCombined_frac{getOci('Wheat')}  = {'Wheat'} ;
+in2out_keyCombined_frac{getOci('Maize')}  = {'Maize'} ;
+in2out_keyCombined_frac{getOci('Sorghum')}  = {'Sorghum'} ;
+in2out_keyCombined_frac{getOci('Rice')}  = {'Rice'} ;
+in2out_keyCombined_frac{getOci('Soybean')}  = {'Soybeans'} ;
+in2out_keyCombined_frac{getOci('FabaBean')}  = {'Pulses'} ;
+out_keys{strcmp(out_names,thisOne)} = in2out_keyCombined_frac ;
+list_cropsCombined_out_ALL{strcmp(out_names,thisOne)} = list_cropsCombined_out ;
+clear in2out_keyCombined_frac
+out_ignores{strcmp(out_names,thisOne)} = ...
+    {'Sugarcane';'Oilpalm';'Citrus';'Datepalm';'GrapesVine';
+    'Cotton';'Cocoa';'Coffee';'OtherAnnuals';'OtherPerennials';
+    'FodderGrasses';'Rye';'Barley';'Millet';
+    'Sunflower';'GroundnutsPeanuts';'RapeseedCanola';
+    'Potatoes';'Sugarbeet';'Cassava'} ;
 
 % Get ignore types
 out_ignore_types = zeros(size(out_ignores)) ;
