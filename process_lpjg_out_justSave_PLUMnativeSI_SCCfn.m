bc562a457fc8048f99bf44d7c19f5e4c3bd3127f
Sam Rabin <sam.rabin@kit.edu>
2019-03-07 16:40:46 +0100

    Reset process_lpjg_out_justSave_PLUMnativeSI_SCCfn() to process all variables.

--- process_lpjg_out_justSave_PLUMnativeSI_SCCfn.m
+++ process_lpjg_out_justSave_PLUMnativeSI_SCCfn.m
@@ -6,38 +6,23 @@ function process_lpjg_out_justSave_PLUMnativeSI_SCCfn( ...
 %%% PLUM-style native %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-%%%% All TRUE except really unnecessary ones
-%do_save.LU              = true ;
-%do_save.crops           = true ;
-%do_save.yield           = true ;
-%do_save.yield_exp       = false ;
-%do_save.yield_map       = true ;
-%do_save.yield_exp_map   = false ;
-%do_save.irrig           = true ;
-%do_save.water           = true ;
-%do_save.carbon          = true ;
-%do_save.mrunoff         = true ;
-%do_save.albedo          = true ;
-%do_save.bvocs           = true ;
-%do_save.Nflux           = true ;
-%do_save.Nfert           = true ;
-%do_save.fpc             = false ;
-
-do_save.LU              = false ;
-do_save.crops           = false ;
-do_save.yield           = false ;
+%%% All TRUE except really unnecessary ones
+do_save.LU              = true ;
+do_save.crops           = true ;
+do_save.yield           = true ;
 do_save.yield_exp       = false ;
-do_save.yield_map       = false ;
+do_save.yield_map       = true ;
 do_save.yield_exp_map   = false ;
-do_save.irrig           = false ;
-do_save.water           = false ;
-do_save.carbon          = false ;
-do_save.mrunoff         = false ;
-do_save.albedo          = false ;
-do_save.bvocs           = false ;
+do_save.irrig           = true ;
+do_save.water           = true ;
+do_save.carbon          = true ;
+do_save.mrunoff         = true ;
+do_save.albedo          = true ;
+do_save.bvocs           = true ;
 do_save.Nflux           = true ;
 do_save.Nfert           = true ;
 do_save.fpc             = false ;
+
 
 %%%%%%%%%%%%%
 %%% Setup %%%
