112adb03d0cc85b0272873953a33632f4a08da2d
Sam Rabin <sam.rabin@kit.edu>
2019-03-21 15:18:16 +0100

    Added some input checks to es_relationship_scatter().

--- es_relationship_scatter.m
+++ es_relationship_scatter.m
@@ -8,6 +8,34 @@ fontSize = 14 ;
 markerSize = 15 ;
 spacing = [0.1 0.1] ;   % [vertical horizontal]
 
+% Check inputs
+if ~any(strcmp(mapstructX.varNames, thisVarX))
+    error('Variable %s not found in mapstructX!', thisVarX)
+elseif ~any(strcmp(mapstructY.varNames, thisVarY))
+    error('Variable %s not found in mapstructY!', thisVarY)
+end
+if ~isfield(mapstructX, 'maps_YXvyB')
+    error('maps_YXvyB not found in mapstructX!')
+elseif ~isfield(mapstructX, 'maps_YXvyr')
+    error('maps_YXvyr not found in mapstructX!')
+elseif ~isfield(mapstructY, 'maps_YXvyB')
+    error('maps_YXvyB not found in mapstructY!')
+elseif ~isfield(mapstructY, 'maps_YXvyr')
+    error('maps_YXvyr not found in mapstructY!')
+end
+size_XB = size(mapstructX.maps_YXvyB(:,:,1,:)) ;
+size_Xr = size(mapstructX.maps_YXvyr(:,:,1,:,:)) ;
+if ~isequal(size_XB(1:2), size(countries_YX))
+    error('~isequal(size_XB(1:2), size(countries_YX))')
+elseif ~isequal(size_Xr(1:2), size(countries_YX))
+    error('~isequal(size_Xr(1:2), size(countries_YX))')
+end
+if ~isequal(size_XB, size(mapstructY.maps_YXvyB(:,:,1,:)))
+    error('~isequal(size_XB, size(mapstructY.maps_YXvyB(:,:,1,:)))')
+elseif ~isequal(size_Xr, size(mapstructY.maps_YXvyr(:,:,1,:,:)))
+    error('~isequal(size_Xr, size(mapstructY.maps_YXvyr(:,:,1,:,:)))')
+end
+
 % Get information about inputs
 countries_list = unique(countries_YX(~isnan(countries_YX))) ;
 Ncountries = length(countries_list) ;
