482706539710430269f7f80da68c90327b3645ed
Sam Rabin <sam.rabin@kit.edu>
2019-03-21 14:55:03 +0100

    Scatter plots now have equalized and labeled X- and Y-axes, and dashed lines are plotted to denote zero values.

--- es_relationship_scatter.m
+++ es_relationship_scatter.m
@@ -1,65 +1,104 @@
 function es_relationship_scatter( ...
-    mapstruct1, thisVar1, ...
-    mapstruct2, thisVar2, ...
+    mapstructX, thisVarX, labelX, ...
+    mapstructY, thisVarY, labelY, ...
     countries_YX, runList)
+
+%%%%% Options
+fontSize = 14 ;
+markerSize = 15 ;
+spacing = [0.1 0.1] ;   % [vertical horizontal]
 
 % Get information about inputs
 countries_list = unique(countries_YX(~isnan(countries_YX))) ;
 Ncountries = length(countries_list) ;
-Nruns = size(mapstruct1.maps_YXvyr,5) ;
+Nruns = size(mapstructX.maps_YXvyr,5) ;
 
 % Get mean for variable in question
-maps1_YXB = mean(mapstruct1.maps_YXvyB(:,:,strcmp(mapstruct1.varNames,thisVar1),:),4) ;
-maps1_YXr = squeeze(mean(mapstruct1.maps_YXvyr(:,:,strcmp(mapstruct1.varNames,thisVar1),:,:),4)) ;
-maps2_YXB = mean(mapstruct2.maps_YXvyB(:,:,strcmp(mapstruct2.varNames,thisVar2),:),4) ;
-maps2_YXr = squeeze(mean(mapstruct2.maps_YXvyr(:,:,strcmp(mapstruct2.varNames,thisVar2),:,:),4)) ;
+maps1_YXB = mean(mapstructX.maps_YXvyB(:,:,strcmp(mapstructX.varNames,thisVarX),:),4) ;
+maps1_YXr = squeeze(mean(mapstructX.maps_YXvyr(:,:,strcmp(mapstructX.varNames,thisVarX),:,:),4)) ;
+maps2_YXB = mean(mapstructY.maps_YXvyB(:,:,strcmp(mapstructY.varNames,thisVarY),:),4) ;
+maps2_YXr = squeeze(mean(mapstructY.maps_YXvyr(:,:,strcmp(mapstructY.varNames,thisVarY),:,:),4)) ;
 
 % Get means for each country
-res1_cB = nan(Ncountries,1) ;
-res2_cB = nan(Ncountries,1) ;
-res1_cr = nan(Ncountries,Nruns) ;
-res2_cr = nan(Ncountries,Nruns) ;
+resX_cB = nan(Ncountries,1) ;
+resY_cB = nan(Ncountries,1) ;
+resX_cr = nan(Ncountries,Nruns) ;
+resY_cr = nan(Ncountries,Nruns) ;
 for c = 1:Ncountries
     thisCountry = countries_list(c) ;
-    res1_cB(c) = nanmean(maps1_YXB(countries_YX==thisCountry)) ;
-    res2_cB(c) = nanmean(maps2_YXB(countries_YX==thisCountry)) ;
+    resX_cB(c) = nanmean(maps1_YXB(countries_YX==thisCountry)) ;
+    resY_cB(c) = nanmean(maps2_YXB(countries_YX==thisCountry)) ;
     for r = 1:Nruns
         tmp_YX = maps1_YXr(:,:,r) ;        
-        res1_cr(c,r) = nanmean(tmp_YX(countries_YX==thisCountry)) ;
+        resX_cr(c,r) = nanmean(tmp_YX(countries_YX==thisCountry)) ;
         tmp_YX = maps2_YXr(:,:,r) ;        
-        res2_cr(c,r) = nanmean(tmp_YX(countries_YX==thisCountry)) ;
+        resY_cr(c,r) = nanmean(tmp_YX(countries_YX==thisCountry)) ;
     end
 end
 
 % Trim countries that are all NaN
-bad_countries = find(isnan(res1_cB) & all(isnan(res1_cr),2) & isnan(res2_cB) & all(isnan(res2_cr),2)) ;
-res1_cB(bad_countries) = [] ;
-res2_cB(bad_countries) = [] ;
-res1_cr(bad_countries,:) = [] ;
-res2_cr(bad_countries,:) = [] ;
-if any(isnan(res1_cB))
-    error('NaN remaining in res1_cB')
-elseif any(any(isnan(res1_cr)))
-    error('NaN remaining in res1_cr')
-elseif any(isnan(res2_cB))
-    error('NaN remaining in res2_cB')
-elseif any(any(isnan(res2_cr)))
-    error('NaN remaining in res2_cr')
+bad_countries = find(isnan(resX_cB) & all(isnan(resX_cr),2) & isnan(resY_cB) & all(isnan(resY_cr),2)) ;
+resX_cB(bad_countries) = [] ;
+resY_cB(bad_countries) = [] ;
+resX_cr(bad_countries,:) = [] ;
+resY_cr(bad_countries,:) = [] ;
+if any(isnan(resX_cB))
+    error('NaN remaining in resX_cB')
+elseif any(any(isnan(resX_cr)))
+    error('NaN remaining in resX_cr')
+elseif any(isnan(resY_cB))
+    error('NaN remaining in resY_cB')
+elseif any(any(isnan(resY_cr)))
+    error('NaN remaining in resY_cr')
 end
 
-% Get diffs
-diff1_cr = res1_cr - repmat(res1_cB,[1 Nruns]) ;
-diff2_cr = res2_cr - repmat(res2_cB,[1 Nruns]) ;
+% Get diffs (future minus historical)
+diff1_cr = resX_cr - repmat(resX_cB,[1 Nruns]) ;
+diff2_cr = resY_cr - repmat(resY_cB,[1 Nruns]) ;
 
 % Make figure
 figure('Color','w','Position',figurePos) ;
-spacing = 0.05 ;
+hs = [] ; % Axis handles
+xmin = NaN ;
+xmax = NaN ;
+ymin = NaN ;
+ymax = NaN ;
 for r = 1:Nruns
-    subplot_tight(2,2,r,spacing)
-    plot(diff1_cr(:,r),diff2_cr(:,r),'.') ;
+    hs(r) = subplot_tight(2,2,r,spacing) ;
+    plot(diff1_cr(:,r),diff2_cr(:,r),'.', ...
+        'MarkerSize',markerSize)
     title(runList{r})
+    xlabel(labelX)
+    ylabel(labelY)
+    set(gca,'FontSize',fontSize)
+    xmin = min(xmin, min(get(gca,'XLim'))) ;
+    ymin = min(ymin, min(get(gca,'YLim'))) ;
+    xmax = max(xmax, max(get(gca,'XLim'))) ;
+    ymax = max(ymax, max(get(gca,'YLim'))) ;
 end
 
+% Equalize axes
+xlims = [xmin xmax] ;
+ylims = [ymin ymax] ;
+for r = 1:Nruns
+    set(hs(r), 'XLim', xlims, 'YLim', ylims)
+end
+
+% Add zero-lines
+if xlims(1)<0 && xlims(2)>0
+    for r = 1:Nruns
+        hold(hs(r),'on')
+        plot(hs(r),[0 0],ylims,'--k')
+        hold(hs(r),'off')
+    end
+end
+if ylims(1)<0 && ylims(2)>0
+    for r = 1:Nruns
+        hold(hs(r),'on')
+        plot(hs(r),xlims,[0 0],'--k')
+        hold(hs(r),'off')
+    end
+end
 
 
 
