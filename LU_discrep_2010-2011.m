f926ca7293ae229a0e71fb90ed58e2024e9472ed
Sam Rabin <sam.rabin@kit.edu>
2019-04-11 16:41:55 +0200

    Apparent disagreement in land use totals between maps and time series was just a result of bad code in the testing script, LU_discrep_2010-2011.m. Resolving #2.

--- LU_discrep_2010-2011.m
+++ LU_discrep_2010-2011.m
@@ -2,11 +2,11 @@ thisLU = 'NATURAL' ;
 
 for r = 1:Nruns
     disp(runList{r})
-    this2010_YX = maps_LU_d9.maps_YXvyr(:,:,strcmp(maps_LU_d9.varNames,thisLU),end,r) ;
+    this2010_YX = maps_LU_d9.maps_YXvyB(:,:,strcmp(maps_LU_d9.varNames,thisLU),end) ;
     this2011_YX = maps_LU_d1.maps_YXvyr(:,:,strcmp(maps_LU_d1.varNames,thisLU),1,r) ;
     disp('From maps:')
-    fprintf('\t2010 %s: %0.3e\n', thisLU, nansum(nansum(this2010_YX.*land_area_YX))) ;
-    fprintf('\t2011 %s: %0.3e\n', thisLU, nansum(nansum(this2011_YX.*land_area_YX))) ;
+    fprintf('\t2010 %s: %0.3e\n', thisLU, nansum(nansum(this2010_YX.*gcel_area_YX))) ;
+    fprintf('\t2011 %s: %0.3e\n', thisLU, nansum(nansum(this2011_YX.*gcel_area_YX))) ;
     disp('From TS:')
     fprintf('\t2010 %s: %0.3e\n', thisLU, ts_LUarea_ntrl_bl(end)) ;
     fprintf('\t2011 %s: %0.3e\n', thisLU, ts_LUarea_ntrl_yr(1,r)) ;
