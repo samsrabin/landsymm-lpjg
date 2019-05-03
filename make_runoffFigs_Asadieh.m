929cd70a87fd98c5a0065beff105a77b80b3ca56
Sam Rabin <sam.rabin@kit.edu>
2019-05-03 14:35:10 +0200

    Drought and flood maps now use last30 instead of d9.

--- make_runoffFigs_Asadieh.m
+++ make_runoffFigs_Asadieh.m
@@ -1,5 +1,5 @@
 function pctDiff_YXr = make_runoffFigs_Asadieh( ...
-    maps_mon_runoff_d9, maps_awater_d9, runList, ...
+    maps_mon_runoff_last30, maps_awater_last30, runList, ...
     droughtOrFlood, ...
     do_norm, spacing, norm_ticks, fontSize, fontSize_text, Ystart)
 
@@ -7,7 +7,7 @@ if ~strcmp(droughtOrFlood, 'drought') && ~strcmp(droughtOrFlood, 'flood')
     error('droughtOrFlood (%s) must be either ''drought'' or ''flood''', droughtOrFlood)
 end
 
-size_YX = [size(maps_mon_runoff_d9.maps_YXvyB,1) size(maps_mon_runoff_d9.maps_YXvyB,2)] ;
+size_YX = [size(maps_mon_runoff_last30.maps_YXvsB,1) size(maps_mon_runoff_last30.maps_YXvsB,2)] ;
 Nruns = length(runList) ;
 BoverA_to_dq = @(BoverA) (BoverA-1)./(BoverA+1) ;
 dq_to_BoverA = @(dq) -(dq+1)./(dq-1) ;
@@ -16,12 +16,15 @@ Ncolors = size(this_colormap,1) ;
 
 % After Asadieh & Krakauer (2017): Ignore cells with runoff < 0.01mm/day in
 % last 10 years of baseline
-ii = strcmp(maps_awater_d9.varNames,'Runoff') ;
-below_thresh_annMean_YX = mean(maps_awater_d9.maps_YXvyB(:,:,ii,:),4)/365 < 0.01 ; 
+ind_var = strcmp(maps_awater_last30.varNames,'Runoff') ;
+ind_stat = strcmp(maps_awater_last30.statList,'last30_mean') ;
+below_thresh_annMean_YX = maps_awater_last30.maps_YXvsB(:,:,ind_var,ind_stat)/365 < 0.01 ; 
 
 if strcmp(droughtOrFlood, 'drought')
-    pXX_q20c_YX = prctile(sum(maps_mon_runoff_d9.maps_YXvyB,3),5,4) ;
-    pXX_q21c_YXr = squeeze(prctile(sum(maps_mon_runoff_d9.maps_YXvyr,3),5,4)) ;
+    ind_var = strcmp(maps_awater_last30.varNames,'Runoff') ;
+    ind_stat = strcmp(maps_awater_last30.statList,'last30_prctile05') ;
+    pXX_q20c_YX = maps_awater_last30.maps_YXvsB(:,:,ind_var,ind_stat) ;
+    pXX_q21c_YXr = squeeze(maps_awater_last30.maps_YXvsr(:,:,ind_var,ind_stat,:)) ;
     below_thresh_YX = below_thresh_annMean_YX ;   % | pXX_q20c_YX / 365 == 0 ; % Not in Asadieh & Krakauer, but otherwise you get Infs...
     pXX_q20c_YX(below_thresh_YX) = NaN ;
     pXX_q21c_YXr(repmat(below_thresh_YX, [1 1 Nruns])) = NaN ;
@@ -29,8 +32,9 @@ if strcmp(droughtOrFlood, 'drought')
     pXX_dq_YXr = (pXX_q21c_YXr - pXX_q20c_YXr) ./ (pXX_q21c_YXr + pXX_q20c_YXr) ;
     thisTitle = '10-year lowest annual runoff' ;
 else
-    pXX_q20c_YX = prctile(reshape(maps_mon_runoff_d9.maps_YXvyB, [size_YX 120]),95,3) ;
-    pXX_q21c_YXr = squeeze(prctile(reshape(maps_mon_runoff_d9.maps_YXvyr,[size_YX 120 Nruns]),95,3)) ;
+    ind_stat = strcmp(maps_mon_runoff_last30.statList,'last30_prctile95') ;
+    pXX_q20c_YX = maps_mon_runoff_last30.maps_YXvsB(:,:,:,ind_stat) ;
+    pXX_q21c_YXr = squeeze(maps_mon_runoff_last30.maps_YXvsr(:,:,:,ind_stat,:)) ;
     below_thresh_YX = below_thresh_annMean_YX ;     % | pXX_q20c_YX * 12/365 < 0.01 ; % Not in Asadieh & Krakauer, but otherwise you get Infs...
     pXX_q20c_YX(below_thresh_YX) = NaN ;
     pXX_q21c_YXr(repmat(below_thresh_YX, [1 1 Nruns])) = NaN ;
