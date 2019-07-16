335b6fdc89cef0eef63ee353186bd0c570cf5490
Sam Rabin <sam.rabin@kit.edu>
2019-07-16 04:42:29 -0500

    Indentation problems seemingly fixed.

--- plum.py
+++ plum.py
@@ -5,109 +5,109 @@ np.random.seed(1234)
 def emulate(K, c, t, w, n, case):
 	#Case: NN- no nitrogen, NNI - No nitrogen irr, N- with nitrogen, NI- with nitrogen irr
 	if (case == 'NN'):
-		Y = (K[0,:,:]              +
-			K[1,:,:]  * c          +
-			K[2,:,:]  * t          +
-			K[3,:,:]  * w          +
-			K[4,:,:]  * c ** 2     +
-			K[5,:,:]  * t ** 2     +
-			K[6,:,:]  * w ** 2     +
-			K[7,:,:]  * c *  w     +
-			K[8,:,:]  * t *  w     +
-			K[9,:,:]  * t ** 3     +
-			K[10,:,:] * w ** 3     +
-			K[11,:,:] * t ** 2 * w +
-			K[12,:,:] * w ** 2 * t  )
+		Y = (K[0,:,:]			  +
+		K[1,:,:]  * c		  +
+		K[2,:,:]  * t		  +
+		K[3,:,:]  * w		  +
+		K[4,:,:]  * c ** 2	 +
+		K[5,:,:]  * t ** 2	 +
+		K[6,:,:]  * w ** 2	 +
+		K[7,:,:]  * c *  w	 +
+		K[8,:,:]  * t *  w	 +
+		K[9,:,:]  * t ** 3	 +
+		K[10,:,:] * w ** 3	 +
+		K[11,:,:] * t ** 2 * w +
+		K[12,:,:] * w ** 2 * t  )
 
 	elif (case == 'NNI'):
-		Y = (K[0,:,:]              + 
-			K[1,:,:]  * c          + 
-			K[2,:,:]  * t          + 
-			K[3,:,:]  * c ** 2     + 
-			K[4,:,:]  * t ** 2     + 
-			K[5,:,:]  * t ** 3      )
+		Y = (K[0,:,:]			  + 
+		K[1,:,:]  * c		  + 
+		K[2,:,:]  * t		  + 
+		K[3,:,:]  * c ** 2	 + 
+		K[4,:,:]  * t ** 2	 + 
+		K[5,:,:]  * t ** 3	  )
 
 	elif (case == 'N'):
-		Y = (K[0,:,:]              + 
-			K[1,:,:]  * c          +
-			K[2,:,:]  * t          +
-			K[3,:,:]  * w          +
-			K[4,:,:]  * n          +
-			K[5,:,:]  * c ** 2     +
-			K[6,:,:]  * t ** 2     +
-			K[7,:,:]  * w ** 2     +
-			K[8,:,:]  * n ** 2     + 
-			K[9,:,:]  * c *  w     +
-			K[10,:,:] * c *  n     +
-			K[11,:,:] * t *  w     +
-			K[12,:,:] * t *  n     +
-			K[13,:,:] * w *  n     +
-			K[14,:,:] * t ** 3     +
-			K[15,:,:] * w ** 3     +
-			K[16,:,:] * t *  w * n +
-			K[17,:,:] * t ** 2 * w +
-			K[18,:,:] * w ** 2 * t +
-			K[19,:,:] * w ** 2 * n +
-			K[20,:,:] * n ** 2 * c +
-			K[21,:,:] * n ** 2 * t +
-			K[22,:,:] * n ** 2 * w  )
+		Y = (K[0,:,:]			  + 
+		K[1,:,:]  * c		  +
+		K[2,:,:]  * t		  +
+		K[3,:,:]  * w		  +
+		K[4,:,:]  * n		  +
+		K[5,:,:]  * c ** 2	 +
+		K[6,:,:]  * t ** 2	 +
+		K[7,:,:]  * w ** 2	 +
+		K[8,:,:]  * n ** 2	 + 
+		K[9,:,:]  * c *  w	 +
+		K[10,:,:] * c *  n	 +
+		K[11,:,:] * t *  w	 +
+		K[12,:,:] * t *  n	 +
+		K[13,:,:] * w *  n	 +
+		K[14,:,:] * t ** 3	 +
+		K[15,:,:] * w ** 3	 +
+		K[16,:,:] * t *  w * n +
+		K[17,:,:] * t ** 2 * w +
+		K[18,:,:] * w ** 2 * t +
+		K[19,:,:] * w ** 2 * n +
+		K[20,:,:] * n ** 2 * c +
+		K[21,:,:] * n ** 2 * t +
+		K[22,:,:] * n ** 2 * w  )
 
 	elif (case == 'NI'):
-		Y = (K[0,:,:]              +
-			K[1,:,:]  * c          +
-			K[2,:,:]  * t          +
-			K[3,:,:]  * n          +
-			K[4,:,:]  * c ** 2     +
-			K[5,:,:]  * t ** 2     +
-			K[6,:,:]  * n ** 2     +
-			K[7,:,:]  * c *  n     +
-			K[8,:,:]  * t *  n     +
-			K[9,:,:]  * t ** 3     +
-			K[10,:,:] * n ** 2 * c +
-			K[11,:,:] * n ** 2 * t  )
+		Y = (K[0,:,:]			  +
+		K[1,:,:]  * c		  +
+		K[2,:,:]  * t		  +
+		K[3,:,:]  * n		  +
+		K[4,:,:]  * c ** 2	 +
+		K[5,:,:]  * t ** 2	 +
+		K[6,:,:]  * n ** 2	 +
+		K[7,:,:]  * c *  n	 +
+		K[8,:,:]  * t *  n	 +
+		K[9,:,:]  * t ** 3	 +
+		K[10,:,:] * n ** 2 * c +
+		K[11,:,:] * n ** 2 * t  )
 
 	elif (case == 'NL'):
-		Y = (K[0,:,:]              + 
-			K[1,:,:]  * c          +
-			K[2,:,:]  * t          +
-			K[3,:,:]  * w          +
-			K[4,:,:]  * n          +
-			K[5,:,:]  * c ** 2     +
-			K[6,:,:]  * t ** 2     +
-			K[7,:,:]  * w ** 2     +
-			K[8,:,:]  * n ** 2     + 
-			K[9,:,:]  * c *  w     +
-			K[10,:,:] * c *  n     +
-			K[11,:,:] * t *  w     +
-			K[12,:,:] * t *  n     +
-			K[13,:,:] * w *  n     +
-			K[14,:,:] * t ** 3     +
-			K[15,:,:] * w ** 3     +
-			K[16,:,:] * t *  w * n +
-			K[17,:,:] * t ** 2 * w +
-			K[18,:,:] * w ** 2 * t +
-			K[19,:,:] * w ** 2 * n +
-			K[20,:,:] * n ** 2 * c +
-			K[21,:,:] * n ** 2 * t +
-			K[22,:,:] * n ** 2 * w +
-			K[23,:,:] * n ** 3      )
+		Y = (K[0,:,:]			  + 
+		K[1,:,:]  * c		  +
+		K[2,:,:]  * t		  +
+		K[3,:,:]  * w		  +
+		K[4,:,:]  * n		  +
+		K[5,:,:]  * c ** 2	 +
+		K[6,:,:]  * t ** 2	 +
+		K[7,:,:]  * w ** 2	 +
+		K[8,:,:]  * n ** 2	 + 
+		K[9,:,:]  * c *  w	 +
+		K[10,:,:] * c *  n	 +
+		K[11,:,:] * t *  w	 +
+		K[12,:,:] * t *  n	 +
+		K[13,:,:] * w *  n	 +
+		K[14,:,:] * t ** 3	 +
+		K[15,:,:] * w ** 3	 +
+		K[16,:,:] * t *  w * n +
+		K[17,:,:] * t ** 2 * w +
+		K[18,:,:] * w ** 2 * t +
+		K[19,:,:] * w ** 2 * n +
+		K[20,:,:] * n ** 2 * c +
+		K[21,:,:] * n ** 2 * t +
+		K[22,:,:] * n ** 2 * w +
+		K[23,:,:] * n ** 3	  )
 
 	elif (case == 'NIL'):
-		Y = (K[0,:,:]              +
-			K[1,:,:]  * c          +
-			K[2,:,:]  * t          +
-			K[3,:,:]  * n          +
-			K[4,:,:]  * c ** 2     +
-			K[5,:,:]  * t ** 2     +
-			K[6,:,:]  * n ** 2     +
-			K[7,:,:]  * c *  n     +
-			K[8,:,:]  * t *  n     +
-			K[9,:,:]  * t ** 3     +
-			K[10,:,:] * n ** 2 * c +
-			K[11,:,:] * n ** 2 * t +
-			K[12,:,:] * n ** 3      )
+		Y = (K[0,:,:]			  +
+		K[1,:,:]  * c		  +
+		K[2,:,:]  * t		  +
+		K[3,:,:]  * n		  +
+		K[4,:,:]  * c ** 2	 +
+		K[5,:,:]  * t ** 2	 +
+		K[6,:,:]  * n ** 2	 +
+		K[7,:,:]  * c *  n	 +
+		K[8,:,:]  * t *  n	 +
+		K[9,:,:]  * t ** 3	 +
+		K[10,:,:] * n ** 2 * c +
+		K[11,:,:] * n ** 2 * t +
+		K[12,:,:] * n ** 3	  )
 
-	Y       = np.nan_to_num(Y)
+	Y	   = np.nan_to_num(Y)
 	Y[Y<0]  = 0
 	Y[Y>30] = 30
 	return(Y)
@@ -128,23 +128,24 @@ def PLUMemulate(GCM, rcp, decade, GGCM, mask_YX, outarray):
 	# Begin strings for outfmt and outheader
 	outfmt="%0.2f %0.2f"
 	outheader = "Lon Lat"
-        
-        # Define PLUM output crops
-        PLUMcrops = ["CerealsC3", "CerealsC4", "Rice", "Oilcrops", "Pulses", "StarchyRoots"];
+	
+	# Define PLUM output crops
+	PLUMcrops = ["CerealsC3", "CerealsC4", "Rice", "Oilcrops", "Pulses", "StarchyRoots"];
 
-        # Define matching to GGCMI crops
-        PLUM2GGCMI_dict={
-        "CerealsC3":    "max_wheat",
-	    "CerealsC4":    "maize",
-	    "Rice":         "rice",
-	    "Oilcrops":     "soy",
-	    "Pulses":       "soy",
-	    "StarchyRoots": "spring_wheat",
-        }
+	# Define matching to GGCMI crops
+	PLUM2GGCMI_dict={
+	#"CerealsC3":	"max_wheat",
+	"CerealsC3":	"winter_wheat",
+	"CerealsC4":	"maize",
+	"Rice":		 "rice",
+	"Oilcrops":	 "soy",
+	"Pulses":	   "soy",
+	"StarchyRoots": "spring_wheat"
+	}
 	if GGCM == "LPJ-GUESS":
-	    PLUM2GGCMI_dict["Rice"] = "spring_wheat"
-	    PLUM2GGCMI_dict["Oilcrops"] = "spring_wheat"
-	    PLUM2GGCMI_dict["StarchyRoots"] = "spring_wheat"
+		PLUM2GGCMI_dict["Rice"] = "spring_wheat"
+		PLUM2GGCMI_dict["Oilcrops"] = "spring_wheat"
+		PLUM2GGCMI_dict["StarchyRoots"] = "spring_wheat"
 
 #	# Define crop name list and dictionary for output crop names
 #	if GGCM == "LPJ-GUESS":
@@ -153,19 +154,19 @@ def PLUMemulate(GCM, rcp, decade, GGCM, mask_YX, outarray):
 #		crops = ["maize" , "rice", "soy", "winter_wheat", "spring_wheat"]
 #	
 #	PLUMcrop_dict={
-#		"maize":        "maiz",
-#		"rice":         "rice",
-#		"soy":          "soya",
+#		"maize":	"maiz",
+#		"rice":		"rice",
+#		"soy":		"soya",
 #		"winter_wheat": "wwhe",
 #		"spring_wheat": "swhe",
 #	}
 
 	for PLUMcrop in PLUMcrops:
-                
+		
 		print("%s rcp%d dec%d %s %s"%(GCM, rcp, decade, GGCM, PLUMcrop))
-                crop = PLUM2GGCMI_dict[PLUMcrop]
+		crop = PLUM2GGCMI_dict[PLUMcrop]
 
-                # Could make this more efficient by not redoing emulation for two PLUMcrops that use the same GGCMI crop
+		# Could make this more efficient by not redoing emulation for two PLUMcrops that use the same GGCMI crop
 
 		# Load Emulator params
 		K  = np.load('/project/ggcmi/AgMIP.output/Jim_Emulator/Sam/crop/%s_%s.npy'%(GGCM, crop))
@@ -174,9 +175,9 @@ def PLUMemulate(GCM, rcp, decade, GGCM, mask_YX, outarray):
 		# Load climate files
 		t  = np.load('/project/ggcmi/AgMIP.output/Jim_Emulator/Sam/climate/rcp%s/tas_%s_%s_rf.npy'%(rcp, GCM, crop))
 		if ((crop == 'winter_wheat') | (crop == 'spring_wheat')): 
-		    tI = t
+			tI = t
 		else: 
-		    tI = np.load('/project/ggcmi/AgMIP.output/Jim_Emulator/Sam/climate/rcp%s/tas_%s_%s_ir.npy'%(rcp, GCM, crop))
+			tI = np.load('/project/ggcmi/AgMIP.output/Jim_Emulator/Sam/climate/rcp%s/tas_%s_%s_ir.npy'%(rcp, GCM, crop))
 		w  = np.load('/project/ggcmi/AgMIP.output/Jim_Emulator/Sam/climate/rcp%s/tas_%s_%s_rf.npy'%(rcp, GCM, crop))
 
 		# CO2 vars
@@ -190,11 +191,11 @@ def PLUMemulate(GCM, rcp, decade, GGCM, mask_YX, outarray):
 		outarray = np.vstack((outarray, rf_60[mask_YX==1]))
 		rf_200 = emulate(K,  co2[decade], t[decade,:,:],  w[decade,:,:], 200, 'N')
 		outarray = np.vstack((outarray, rf_200[mask_YX==1]))
-		ir_10  = emulate(KI, co2[decade], tI[decade,:,:], 1,             10,  'NI')
+		ir_10  = emulate(KI, co2[decade], tI[decade,:,:], 1, 10,  'NI')
 		outarray = np.vstack((outarray, ir_10[mask_YX==1]))
-		ir_60  = emulate(KI, co2[decade], tI[decade,:,:], 1,             60,  'NI')
+		ir_60  = emulate(KI, co2[decade], tI[decade,:,:], 1, 60,  'NI')
 		outarray = np.vstack((outarray, ir_60[mask_YX==1]))
-		ir_200 = emulate(KI, co2[decade], tI[decade,:,:], 1,             200, 'NI')
+		ir_200 = emulate(KI, co2[decade], tI[decade,:,:], 1, 200, 'NI')
 		outarray = np.vstack((outarray, ir_200[mask_YX==1]))
 		#ir_200 = ir_200[mask_YX==1]
 
@@ -221,34 +222,36 @@ lats = lats_YX[mask_YX==1]
 lonlats = np.vstack((lons,lats))
 
 for decade in decades:
-    for rcp in rcps:
-        for GCM in GCMs:
-            for GGCM in GGCMs:
+	for rcp in rcps:
+		for GCM in GCMs:
+			for GGCM in GGCMs:
 
-                # Set up info about this run
-                decade_str = str(2011+10*decade) + "-" + str(2020+10*decade)
-                print("%s rcp%d %s %s..."%(GCM, rcp, decade_str, GGCM))
-                outdir = "/project/ggcmi/AgMIP.output/Jim_Emulator/Sam/yields.nowheatIR/" + GCM + "/rcp" + str(rcp) + "/" + GGCM + "/" + decade_str + "/"
-                #outfile = outdir + GCM + "_rcp" + str(rcp) + "_" + GGCM + "_" + crop + "_" + decade_str + ".csv"
-                outfile = outdir + "yield.nowheatIR." + decade_str + ".out"
-                
-                # Make output directory, if needed
-                try:
-                	os.makedirs(outdir)
-                	print("mkdir -p " + outdir)
-                except FileExistsError:
-                	# directory already exists
-                	pass
-                
-                # Emulate
-                outarray,outheader,outfmt = PLUMemulate(GCM, rcp, decade, GGCM, mask_YX, lonlats)
-                
-                # Save in PLUM-readable format
-                np.savetxt(outfile, outarray.T,
-                	delimiter=" ",
-                	fmt=outfmt,
-                	header=outheader,
-                	comments="")
+				# Set up info about this run
+				decade_str = str(2011+10*decade) + "-" + str(2020+10*decade)
+				print("%s rcp%d %s %s..."%(GCM, rcp, decade_str, GGCM))
+				outdir = "/project/ggcmi/AgMIP.output/Jim_Emulator/Sam/yields.nowheatIR/" + GCM + "/rcp" + str(rcp) + "/" + GGCM + "/" + decade_str + "/"
+				#outfile = outdir + GCM + "_rcp" + str(rcp) + "_" + GGCM + "_" + crop + "_" + decade_str + ".csv"
+				outfile = outdir + "yield.nowheatIR." + decade_str + ".out"
+				
+				# Make output directory, if needed
+				try:
+					os.makedirs(outdir)
+					print("mkdir -p " + outdir)
+				except FileExistsError:
+					# directory already exists
+					pass
+				
+				# Emulate
+				outarray,outheader,outfmt = PLUMemulate(GCM, rcp, decade, GGCM, mask_YX, lonlats)
+				
+				# Save in PLUM-readable format
+				np.savetxt(outfile, outarray.T,
+				delimiter=" ",
+				fmt=outfmt,
+				header=outheader,
+				comments="")
+
+
 
 
 
