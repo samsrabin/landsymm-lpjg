750c5d911882a7742eedc215e424787e3760720f
Sam Rabin <sam.rabin@kit.edu>
2019-07-24 11:06:07 +0200

    Updated get_kcalDensity2() to recognize typical LPJ-GUESS CFT abbreviations.

--- get_kcalDensity2.m
+++ get_kcalDensity2.m
@@ -6,17 +6,24 @@ function kcal_per_g = get_kcalDensity2(thisCrop_orig)
 % kcal_per_g = kcal_per_g_wet/dry_matter_frac
 
 thisCrop = lower(thisCrop_orig) ;
+if strcmp(thisCrop(end-2:end),'irr')
+    thisCrop = thisCrop(1:end-3) ;
+end
 
 switch thisCrop
-    case 'cerealsc3';     kcal_per_g = 3.34/0.88 ; % Wheat
-    case 'cerealsc4';     kcal_per_g = mean([3.56/0.88 3.40/0.88]) ; % Maize, millet
+    case {'cerealsc3', 'teww', 'tesw'}
+        kcal_per_g = 3.34/0.88 ; % Wheat
+    case {'cerealsc4', 'teco'}
+        kcal_per_g = mean([3.56/0.88 3.40/0.88]) ; % Maize, millet
     case 'cereals';       kcal_per_g = mean([3.34/0.88 3.56/0.88 3.40/0.88]) ; % Wheat, maize, millet
     case 'oilcrops';      kcal_per_g = mean([3.08/0.93 3.35/0.90 4.14/0.94 4.94/0.92]) ; % Sunflower, soybean, groundnut, rapeseed
     case 'pulses';        kcal_per_g = 3.41/0.90 ; % Pulses
-    case 'rice';          kcal_per_g = 2.80/0.87 ; % Rice
+    case {'rice', 'trri'}
+        kcal_per_g = 2.80/0.87 ; % Rice
     case 'starchyroots';  kcal_per_g = mean([0.70/0.24 1.09/0.35]) ; % Sugar beet, cassava
     case {'cc3g','cc4g'}; kcal_per_g = 0 ;
-    otherwise ; error(['thisCrop (' thisCrop_orig ') not recognized for calorie density!']) ;
+    otherwise
+        error(['thisCrop (' thisCrop_orig ') not recognized for calorie density!']) ;
 end
 
 end
