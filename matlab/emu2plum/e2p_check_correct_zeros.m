0e55be9a55e0c2b54cf777a2c85fc65106d555e8
Sam Rabin <sam.rabin@kit.edu>
2020-03-13 10:29:07 +0100

    Minor text fix in matlab/emu2plum/e2p_check_correct_zeros.m. Also added comments there.

--- matlab/emu2plum/e2p_check_correct_zeros.m
+++ matlab/emu2plum/e2p_check_correct_zeros.m
@@ -1,4 +1,9 @@
 function e2p_check_correct_zeros(data_gvt, which_file, basenamesi)
+% If which_file is yield, returns error if no positive cells found for one
+% or more crops.
+% If which_file is gsirrigation, returns error if no positive cells found
+% for one or more irrigated crops, or if any positive cells are found for
+% one or more rainfed crops.
 
 if strcmp(which_file, 'yield')
     test_gt0_is_good(data_gvt)
@@ -37,7 +42,7 @@ for t = 1:size(data_gvt, 3)
     if any(any(data_gvt(:,:,t)>0,1))
         Ifound = find(~any(data_gvt(:,:,t)>0,1)) ;
         Nfound = length(Ifound) ;
-        error('No positive cells found for %d/%d variables (first %d) at timestep %d', ...
+        error('Positive cells found for %d/%d variables (first %d) at timestep %d', ...
             Nfound, size(data_gvt, 2), Ifound(1), t)
     end
 end
