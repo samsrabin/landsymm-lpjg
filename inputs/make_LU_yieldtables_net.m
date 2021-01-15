1354fb13a8785b0b7c33537c8138ff8e51f0fb02
Sam Rabin <sam.rabin@kit.edu>
2021-01-15 15:05:26 +0100

    inputs/make_LU_yieldtables_net.m can now split among different land uses at any given year.

--- inputs/make_LU_yieldtables_net.m
+++ inputs/make_LU_yieldtables_net.m
@@ -3,11 +3,11 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 % Starts as NATURAL in transitions{1,1}-1. Then produces rows to ensure
-% non-interpolated 100% transitions to subsequent land uses in specified
+% non-interpolated transitions to subsequent land uses in specified
 % years.
 transitions = { ...
     1970, 'CROPLAND' ;
-    1980, 'FOREST' ;
+    1980, {'CROPLAND','FOREST'} ;
     } ;
 
 dir_out = '/Volumes/Reacher/LandSyMM/inputs/LU' ;
@@ -17,7 +17,16 @@ dir_out = '/Volumes/Reacher/LandSyMM/inputs/LU' ;
 
 Ntrans = size(transitions,1) ;
 
-LU_list = unique([{'NATURAL'} ; transitions(:,2)], 'stable') ;
+LU_list = {'NATURAL'} ;
+for t = 1:Ntrans
+    tmp = transitions{t,2} ;
+    if iscell(tmp)
+        LU_list = [LU_list tmp] ; %#ok<AGROW>
+    else
+        LU_list = [LU_list {tmp}] ; %#ok<AGROW>
+    end
+end
+LU_list = unique(LU_list, 'stable') ;
 Nlu = length(LU_list) ;
 
 yearList = ((transitions{1,1}-1):transitions{end,1})' ;
@@ -30,11 +39,23 @@ LU_yv = zeros(Nyears,Nlu) ;
 for t = 0:Ntrans
     if t==0
         LU_yv(1, strcmp(LU_list, 'NATURAL')) = 1 ;
-    elseif t==Ntrans
-        LU_yv(end, strcmp(LU_list, transitions{t,2})) = 1 ;
     else
-        [~,IA] = intersect(yearList,transitions{t,1}:(transitions{t+1,1})-1) ;
-        LU_yv(IA, strcmp(LU_list, transitions{t,2})) = 1 ;
+        LU_list_now = transitions{t,2} ;
+        [~, Ilu] = intersect(LU_list, LU_list_now) ;
+        if ~iscell(LU_list_now)
+            LU_list_now = {LU_list_now} ;
+        end
+        Nlu_now = length(LU_list_now) ;
+        if length(Ilu) ~= Nlu_now
+            error('Error finding land uses: Found %d, expected %d', ...
+                length(Ilu), Nlu_now)
+        end
+        if t==Ntrans
+            IA = Nyears ;
+        else
+            [~,IA] = intersect(yearList,transitions{t,1}:(transitions{t+1,1})-1) ;
+        end
+        LU_yv(IA, Ilu) = 1/Nlu_now ;
     end
 end
 
@@ -49,8 +70,17 @@ file_out = sprintf('%s/landuse-totals', dir_out) ;
 sep = '.' ;
 for t = 1:Ntrans
     thisLU = transitions{t,2} ;
+    if iscell(thisLU)
+        thisLetter = '' ;
+        for x = 1:length(thisLU)
+            tmp = thisLU{x} ;
+            thisLetter = [thisLetter lower(tmp(1))] ;
+        end
+    else
+        thisLetter = lower(thisLU(1)) ;
+    end
     file_out = sprintf('%s%s%s%d', ...
-        file_out, sep, lower(thisLU(1)), transitions{t,1}) ;
+        file_out, sep, thisLetter, transitions{t,1}) ;
     sep = '-' ;
 end
 file_out = sprintf('%s.txt', ...
