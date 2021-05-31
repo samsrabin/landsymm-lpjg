c2973028ffbe679128802c09f4593ed7d11891bd
Sam Rabin <sam.rabin@kit.edu>
2021-05-31 14:09:44 +0200

    Functionized some things in e2p_fake_1000().

--- matlab/emu2plum/e2p_fake_1000.m
+++ matlab/emu2plum/e2p_fake_1000.m
@@ -36,58 +36,15 @@ for c = 1:Ncrops
         thisCrop_200_emu = [thisCropIrr N200_str_emu] ;
         thisCrop_1000 = [thisCropIrr N1000_str_lpj] ;
         
-        % Find stand with 200 kgN/ha in LPJ-GUESS
-        ind_lpj_200 = find( ...
-            strcmp(getbasenamei(data_fu_lpj.varNames), thisCropIrr) ...
-            & getN_num(data_fu_lpj.varNames)==200) ;
-        if length(ind_lpj_200) ~= 1
-            error('Error finding thisCrop_200: %d found', length(ind_lpj_200))
-        end
+        % Find stands
+        ind_lpj_200 = find_stand(data_fu_lpj.varNames, thisCropIrr, 200) ;
+        ind_lpj_1000 = find_stand(data_fu_lpj.varNames, thisCropIrr, 1000) ;
+        ind_out_200 = find_stand(data_fu_out.varNames, thisCropIrr, 200) ;
+        ind_out_1000 = find_stand(data_fu_out.varNames, thisCropIrr, 1000) ;
         
-        % Find stand with 1000 kgN/ha in LPJ-GUESS
-        ind_lpj_1000 = find( ...
-            strcmp(getbasenamei(data_fu_lpj.varNames), thisCropIrr) ...
-            & getN_num(data_fu_lpj.varNames)==1000) ;
-        if length(ind_lpj_1000) ~= 1
-            error('Error finding ind_lpj_1000: %d found', length(ind_lpj_1000))
-        end
-        
-        % Find stand with 200 kgN/ha in output
-        ind_out_200 = find( ...
-            strcmp(getbasenamei(data_fu_out.varNames), thisCropIrr) ...
-            & getN_num(data_fu_out.varNames)==200) ;
-        if length(ind_out_200) ~= 1
-            error('Error finding ind_out_200: %d found', length(ind_out_200))
-        end
-        
-        % Find stand with 1000 kgN/ha in output
-        ind_out_1000 = find( ...
-            strcmp(getbasenamei(data_fu_out.varNames), thisCropIrr) ...
-            & getN_num(data_fu_out.varNames)==1000) ;
-        if length(ind_out_1000) ~= 1
-            error('Error finding ind_out_1000: %d found', length(ind_out_1000))
-        end
-                
         % Calculate deltas
-        delta_x1t = data_fu_lpj.garr_xvt(:,ind_lpj_1000,:) ./ data_fu_lpj.garr_xvt(:,ind_lpj_200,:) ;
-        delta_x1t(data_fu_lpj.garr_xvt(:,ind_lpj_200,:)==0 & data_fu_lpj.garr_xvt(:,ind_lpj_1000,:)==0) = 1 ;
-        
-        % If any infinite, first try excluding very small values
-        if any(isinf(delta_x1t))
-            if strcmp(which_file, 'yield')
-                small_thresh = 0.001 ; % 0.001 kgC/m2 yield
-            elseif strcmp(which_file, 'gsirrigation')
-                small_thresh = 20 ; % 20 mm irrigation
-            else
-                error('which_file (%s) not recognized for e2p_fake_1000().', which_file)
-            end
-            is_small = isinf(delta_x1t) & ...
-                data_fu_lpj.garr_xvt(:,ind_lpj_1000,:) <= small_thresh ;
-            delta_x1t(is_small) = 1 ;
-        end
-        if any(isinf(delta_x1t))
-            error('Infs remain in delta_x1t. Deal with these.')
-        end
+        delta_x1t = calculate_deltas( ...
+            data_fu_lpj, ind_lpj_200, ind_lpj_1000, which_file) ;
         
         % Multiply onto existing N200 to get fake N1000
         thisInd = (c-1)*Nirr + ii ;
@@ -104,5 +61,43 @@ end
 e2p_check_correct_zeros(data_fu_out.garr_xvt, ...
     which_file, data_fu_out.varNames, ...
     'Future', @getbasenamei)
+    
+end
+
+
+function delta_x1t = calculate_deltas(data_fu, ind_loN, ind_hiN, which_file)
+
+delta_x1t = data_fu.garr_xvt(:,ind_hiN,:) ./ data_fu.garr_xvt(:,ind_loN,:) ;
+delta_x1t(data_fu.garr_xvt(:,ind_loN,:)==0 & data_fu.garr_xvt(:,ind_hiN,:)==0) = 1 ;
+
+% If any infinite, first try excluding very small values
+if any(isinf(delta_x1t))
+    if strcmp(which_file, 'yield')
+        small_thresh = 0.001 ; % 0.001 kgC/m2 yield
+    elseif strcmp(which_file, 'gsirrigation')
+        small_thresh = 20 ; % 20 mm irrigation
+    else
+        error('which_file (%s) not recognized for e2p_fake_1000().', which_file)
+    end
+    is_small = isinf(delta_x1t) & ...
+        data_fu.garr_xvt(:,ind_hiN,:) <= small_thresh ;
+    delta_x1t(is_small) = 1 ;
+end
+if any(isinf(delta_x1t))
+    error('Infs remain in delta_x1t. Deal with these.')
+end
+
+end
+
+
+function ind = find_stand(varNames, thisCropIrr, thisN)
+
+ind = find( ...
+    strcmp(getbasenamei(varNames), thisCropIrr) ...
+    & getN_num(varNames)==thisN) ;
+if length(ind) ~= 1
+    error('Error finding %s: %d found', ...
+        thisCropIrr, length(ind))
+end
 
 end
