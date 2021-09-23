2b45a745486114baf55f8004410e57ae31d412d9
Sam Rabin <sam.rabin@gmail.com>
2021-09-23 16:11:27 -0600

    New version reflecting recent changes: 20210923.

--- matlab/emu2plum/e2p__parent.m
+++ matlab/emu2plum/e2p__parent.m
@@ -49,7 +49,7 @@ gcm_list = {'UKESM1-0-LL'} ;
 ggcm_list = {'EPIC-TAMU', 'pDSSAT', 'GEPIC'} ;
 % ggcm_list = {'EPIC-TAMU'} ;
 ssp_list = {'ssp126'} ;
-thisVer = '20210702' ;
+thisVer = '20210923' ;
 emuVer = 'v2.5' ;
 adaptation = 1 ;
 
