91cd9c9550412deaa3b2d2061ef483816e7f8734
Sam Rabin <sam.rabin@gmail.com>
2021-09-24 19:37:36 -0600

    Added percentile-based fertilizer-yield curve outputs.

--- matlab/emu2plum/e2p__fertyieldcurves.m
+++ matlab/emu2plum/e2p__fertyieldcurves.m
@@ -117,6 +117,14 @@ data_fu.tsN_list = tsN_list ;
 
 disp('Done.')
 
+%% What is the maximum yield seen?
+
+[M, I] = max(data_fu.garr_xvtg(:)) ;
+[x, v, t, g] = ind2sub(size(data_fu.garr_xvtg), I) ;
+fprintf('%s %s %d-%d: %g t/ha\n', ...
+    ggcm_list{g}, data_fu.varNames{v}, ts1_list(t), tsN_list(t), ...
+    M*10)
+
 
 %% Import land use
 disp('Importing land use...')
@@ -185,7 +193,7 @@ clear cropfracs LU
 disp('Done.')
 
 
-%% Make curves: Just Nth time period, all crops in one figure
+%% Make curves: Just Nth time period, all crops in one figure, area-wtd
 warning off export_fig:exportgraphics
 
 % Which time period?
@@ -198,31 +206,84 @@ lineWidth = 1.25 ;
 
 thisCO = colororder ;
 
-make_figure(cropareas, data_fu, t, ggcm_list, cropList, irrList, ...
-    thisPos, fontSize, lineWidth, thisCO, cfs_cg)
-export_fig(sprintf('%s/fertyield_%d-%d.png', ...
-    outDir, data_fu.ts1_list(t), data_fu.tsN_list(t)), '-r200')
-close
+output_units = {'prod', 'yield'} ;
 
-for g = 1:Nggcm
-    thisGGCM = ggcm_list{g} ;
-    fprintf('%s...\n', thisGGCM)
-    data_fu_tmp = data_fu ;
-    data_fu_tmp.garr_xvtg = data_fu.garr_xvtg(:,:,:,g) ;
-    make_figure(cropareas, data_fu_tmp, t, ggcm_list(g), cropList, irrList, ...
-        thisPos, fontSize, lineWidth, thisCO(g,:), cfs_cg)
-    export_fig(sprintf('%s/fertyield_%d-%d_%s.png', ...
-        outDir, data_fu.ts1_list(t), data_fu.tsN_list(t), ...
-        thisGGCM), '-r200')
+for u = 1:length(output_units)
+    
+    prod_or_yield = output_units{u} ;
+
+    make_figure_areaweighted(cropareas, data_fu, t, ggcm_list, cropList, irrList, ...
+        thisPos, fontSize, lineWidth, thisCO, cfs_cg, prod_or_yield)
+    export_fig(sprintf('%s/fertyield_%s_%d-%d.png', ...
+        outDir, prod_or_yield, data_fu.ts1_list(t), data_fu.tsN_list(t)), ...
+        '-r200')
     close
+
+%     for g = 1:Nggcm
+%         thisGGCM = ggcm_list{g} ;
+%         fprintf('%s...\n', thisGGCM)
+%         data_fu_tmp = data_fu ;
+%         data_fu_tmp.garr_xvtg = data_fu.garr_xvtg(:,:,:,g) ;
+%         make_figure_areaweighted(cropareas, data_fu_tmp, t, ggcm_list(g), cropList, irrList, ...
+%             thisPos, fontSize, lineWidth, thisCO(g,:), cfs_cg, prod_or_yield)
+%         export_fig(sprintf('%s/fertyield_%s_%d-%d_%s.png', ...
+%             outDir, prod_or_yield, data_fu.ts1_list(t), data_fu.tsN_list(t), ...
+%             thisGGCM), '-r200')
+%         close
+%     end
 end
+disp('Done!')
+
+
+%% Make curves: Each time period, all crops in one figure, percentile
+warning off export_fig:exportgraphics
+
+% What percentile of yield?
+this_prctile = 100 ;
+% Which time period?
+% t = 2 ;
+t = 8 ;
+% Figure options
+thisPos = figurePos ;
+fontSize = 12 ;
+lineWidth = 1.25 ;
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+thisCO = colororder ;
+
+for t = 1:Ntpers
+
+    make_figure_allcellsequal(data_fu, t, ggcm_list, cropList, irrList, ...
+        thisPos, fontSize, lineWidth, thisCO, cfs_cg, this_prctile)
+    export_fig(sprintf('%s/fertyield_%g-pctleYield_%d-%d.png', ...
+        outDir, this_prctile, data_fu.ts1_list(t), data_fu.tsN_list(t)), ...
+        '-r200')
+    close
+    
+%     for g = 1:Nggcm
+%         thisGGCM = ggcm_list{g} ;
+%         fprintf('%s...\n', thisGGCM)
+%         data_fu_tmp = data_fu ;
+%         data_fu_tmp.garr_xvtg = data_fu.garr_xvtg(:,:,:,g) ;
+%         make_figure_allcellsequal(data_fu_tmp, t, ggcm_list(g), cropList, irrList, ...
+%             thisPos, fontSize, lineWidth, thisCO(g,:), cfs_cg, this_prctile)
+%         export_fig(sprintf('%s/fertyield_%g-pctleYield_%d-%d_%s.png', ...
+%             outDir, this_prctile, data_fu.ts1_list(t), data_fu.tsN_list(t), ...
+%             thisGGCM), '-r200')
+%         close
+%     end
+
+end
+
+
+    
 disp('Done!')
 
 
 %% FUNCTIONS
 
-function make_figure(cropareas, data_fu, t, ggcm_list, cropList, irrList, ...
-    thisPos, fontSize, lineWidth, thisCO, cfs_cg)
+function make_figure_areaweighted(cropareas, data_fu, t, ggcm_list, cropList, irrList, ...
+    thisPos, fontSize, lineWidth, thisCO, cfs_cg, prod_or_yield)
 
 Nggcm = length(ggcm_list) ;
 Ncrops = length(cropList) ;
@@ -279,11 +340,20 @@ for cc = 1:Ncrops+1
             I_is_thisCropIrr = I_is_thisCropIrr(I_sort) ;
 %             varNames_thisCropIrr = varNames_thisCropIrr(I_sort) ;
             
-            % Get values; convert t to Mt
+            % Get values
             data_thisCropIrr_n = nansum( ...
                 data_thisCrop_xvg(:,I_is_thisCropIrr,g) ...
                 .* thisCrop_area_x, ...
-                1) * 1e-6 ;
+                1) ;
+            if strcmp(prod_or_yield, 'prod')
+                % Convert t to Mt
+                data_thisCropIrr_n = data_thisCropIrr_n * 1e-6 ;
+            elseif strcmp(prod_or_yield, 'yield')
+                data_thisCropIrr_n = data_thisCropIrr_n / sum(thisCrop_area_x) ;
+            else
+                error('prod_or_yield %s not recognized', prod_or_yield)
+            end
+            
             % Multiply by calibration factors
             data_thisCropIrr_n = data_thisCropIrr_n ...
                 * cfs_cg(c,g) ;
@@ -328,7 +398,133 @@ for cc = 1:Ncrops+1
         title(sprintf('%s %d-%d', ...
             thisCrop, data_fu.ts1_list(t), data_fu.tsN_list(t)))
         xlabel('Fertilization (kg/ha)')
-        ylabel('Production (Mt)')
+        if strcmp(prod_or_yield, 'prod')
+            ylabel('Production (Mt)')
+        elseif strcmp(prod_or_yield, 'yield')
+            ylabel('Yield (t/ha)')
+        else
+            error('prod_or_yield %s not recognized', prod_or_yield)
+        end
+    end
+end % Loop through crops
+
+axis off
+legend(theLegend, ...
+    'Location', 'west', ...
+    'FontSize', fontSize + 2) ;
+
+end
+
+
+function make_figure_allcellsequal(data_fu, t, ggcm_list, cropList, irrList, ...
+    thisPos, fontSize, lineWidth, thisCO, cfs_cg, this_prctile)
+
+Nggcm = length(ggcm_list) ;
+Ncrops = length(cropList) ;
+
+theLegend = [ ...
+    strcat(ggcm_list, ' (rainfed)') ;
+    strcat(ggcm_list, ' (irrigated)') ...
+    ] ;
+theLegend = theLegend(:) ;
+
+figure('Color', 'w', 'Position', thisPos)
+tiledlayout('flow')
+
+for cc = 1:Ncrops+1
+    c = min(cc,Ncrops) ;
+    is_fakeplot = cc==Ncrops+1 ;    
+    
+    thisCrop = cropList{c} ;
+    is_thisCrop = contains( ...
+        data_fu.varNames, thisCrop) ;
+    varNames_thisCrop = ...
+        data_fu.varNames(is_thisCrop) ;
+    data_thisCrop_xvg = ...
+        data_fu.garr_xvtg(:,is_thisCrop,t,:) ;
+        
+    nexttile
+    for g = 1:Nggcm
+        
+        for ii = 1:length(irrList)
+            
+            % Rainfed dashed, irrigated solid
+            if ii == 1
+                lineMarkerStyle = '--o' ;
+                markerSize = 6.5 ;
+            else
+                lineMarkerStyle = '-o' ;
+                markerSize = 6.5 ;
+            end
+            
+            % Get and sort data for this crop*irrigation
+            thisCropIrr = [thisCrop irrList{ii}] ;
+            is_thisCropIrr = ~cellfun(@isempty, regexp( ...
+                varNames_thisCrop, ...
+                [thisCropIrr '\d+'])) ;
+            varNames_thisCropIrr = ...
+                varNames_thisCrop(is_thisCropIrr) ;
+            N_thisCropIrr = ...
+                getN_num(varNames_thisCropIrr) ;
+            I_is_thisCropIrr = find(is_thisCropIrr) ;
+            [N_thisCropIrr, I_sort] = sort(N_thisCropIrr) ;
+            I_is_thisCropIrr = I_is_thisCropIrr(I_sort) ;
+%             varNames_thisCropIrr = varNames_thisCropIrr(I_sort) ;
+            
+            % Get values
+            data_thisCropIrr_n = prctile( ...
+                data_thisCrop_xvg(:,I_is_thisCropIrr,g), ...
+                this_prctile, ...
+                1) ;
+            
+            % Convert to t/ha
+            data_thisCropIrr_n = 10 * data_thisCropIrr_n ;
+            
+            % Multiply by calibration factors
+            data_thisCropIrr_n = data_thisCropIrr_n ...
+                * cfs_cg(c,g) ;
+            
+            % Make fakeplot lines invisible
+            if is_fakeplot
+                data_thisCropIrr_n = nan(size(data_thisCropIrr_n)) ;
+            end
+            
+            hold on
+%             set(gca, 'ColorOrderIndex', g) ;
+            thisColor = thisCO(g,:) ;
+            hl = plot(N_thisCropIrr, ...
+                data_thisCropIrr_n, ...
+                lineMarkerStyle, ...
+                'MarkerSize', markerSize, ...
+                'LineWidth', lineWidth, ...
+                'Color', thisColor, ...
+                'MarkerEdgeColor', thisColor) ;
+            set(gca, ...
+                'XTick', N_thisCropIrr, ...
+                'FontSize', fontSize)
+            if ii == 2
+                set(hl, 'MarkerFaceColor', thisColor)
+            end
+            
+            % Add calibration factor text
+            if length(ggcm_list) == 1 && ii==1 && ~is_fakeplot
+                thisText = sprintf('Calib. factor = %0.3f', ...
+                    cfs_cg(c,g)) ;
+                text(0.5, 0.05, thisText, ...
+                    'FontSize', fontSize, ...
+                    'Units', 'normalized', ...
+                    'HorizontalAlignment', 'center') ;
+            end
+            
+            hold off
+            
+        end % Loop through irrigation
+    end % Loop through GGCMs
+    if ~is_fakeplot
+        title(sprintf('%s %d-%d: %g percentile yield', ...
+            thisCrop, data_fu.ts1_list(t), data_fu.tsN_list(t), this_prctile))
+        xlabel('Fertilization (kg/ha)')
+        ylabel('Yield (t/ha)')
     end
 end % Loop through crops
 
