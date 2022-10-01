c2a903b5515b2de8c2165c51fdb34612e3ec5c54
Sam Rabin <sam.rabin@gmail.com>
2022-10-01 13:57:59 -0600

    remapv10_old_62892_gL.m now expands LPJ-GUESS soil to match gridlist.

--- inputs/remapv10_old_62892_gL.m
+++ inputs/remapv10_old_62892_gL.m
@@ -658,6 +658,50 @@ clear out_nfert_array
 diary off
 
 
+%% Interpolate and save soil
+
+file_soil = '/Users/Shared/lpj-guess/input/soil/WISE/soilmap_center_interpolated.dat' ;
+soil = lpjgu_matlab_read2geoArray(file_soil, ...
+    'verboseIfNoMat', false, 'force_mat_save', false, 'force_mat_nosave', true) ; 
+
+out_soil.varNames = soil.varNames ;
+out_soil.lonlats = gridlist.lonlats ;
+out_soil.list2map = gridlist.list2map ;
+Nvars = length(soil.varNames) ;
+out_soil.garr_xv = nan(Ncells, Nvars) ;
+for v = 1:Nvars
+    fprintf('Interpolating soil %s...\n', soil.varNames{v})
+    tmp0_YX = lpjgu_xz_to_YXz(soil.garr_xv(:,v), size(gridlist.mask_YX), soil.list2map) ;
+    tmp1_YX = inpaint_nans(tmp0_YX, inpaint_method) ;
+    out_soil.garr_xv(:,v) = tmp1_YX(gridlist.list2map) ;
+end
+disp('Done.')
+
+if any(any(isnan(out_soil.garr_xv)))
+    error('NaN in out_soil.garr_xv')
+elseif any(any(out_soil.garr_xv < 0))
+    error('Negative in out_soil.garr_xv')
+end
+
+out_soil_header_cell = [ ...
+    {'lon', 'lat'}, ...
+    out_soil.varNames] ;
+
+[~, soil_filename, soil_ext] = fileparts(file_soil) ;
+out_file_soil = sprintf('%s%s.remapv%s%s', out_dir, soil_filename, remapVer, soil_ext) ;
+
+disp('Saving soil...')
+lpjgu_matlab_saveTable(out_soil_header_cell, out_soil, out_file_soil,...
+    'outPrec', 3, ...
+    'outWidth', outWidth, ...
+    'delimiter', delimiter, ...
+    'overwrite', overwrite, ...
+    'fancy', fancy, ...
+    'progress_step_pct', 20, ...
+    'gzip', do_gzip) ;
+
+
+
  %% FUNCTIONS
  
 function out_array = coarsen_res(in_array,in_res,out_res)
@@ -690,7 +734,7 @@ end
  end
 
  
- function out_array = coarsen_res_mgmt(in_array,inArea_array,in_res,out_res)
+function out_array = coarsen_res_mgmt(in_array,inArea_array,in_res,out_res)
 
 Ndims_in = length(find(size(in_array)>1)) ;
 if Ndims_in>4
