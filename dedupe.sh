db3a30c430f7cb4c3e0a1c45f40a322156288e29
Sam Rabin <sam.rabin@gmail.com>
2022-12-20 15:18:46 -0500

    Added script to help find duplicate filenames.

---
+++ dedupe.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -e
+
+d1=/Users/sam/Documents/Dropbox/2016_KIT/LandSyMM/MATLAB_work
+d2=/Users/sam/Documents/git_repos/g2p_emulation
+
+for f in make_justCPB.m make_himelo_filled.m make_himelo.m removeCols_addZeros.m get_remapv2_keys.m; do
+	f1=$(find $d1 -name $f)
+	f2=$(find $d2 -name $f)
+	echo $f
+	ls -lh $f1
+	ls -lh $f2
+	echo " "
+done
+
+exit 0
