d432179549a7962445cea17fee3ee43e28a50335
Sam Rabin <sam.rabin@gmail.com>
2023-11-18 11:42:38 -0600

    Fixes in PLUMharm_check_dirs().

--- plum_harmonization/PLUMharm_check_dirs.m
+++ plum_harmonization/PLUMharm_check_dirs.m
@@ -4,20 +4,29 @@ Ndirs = length(dirList) ;
 
 for d = 1:Ndirs
     thisDir = dirList{d} ;
+
+    if ~exist(thisDir, 'dir')
+        if ~contains(rw, 'w')
+            error('%s not found', ...
+                thisDir)
+        end
+        mkdir(thisDir)
+    end
+
     [~, fa] = fileattrib(thisDir) ;
 
     % Get absolute path
     thisDir = fa.Name ;
 
     % Check
-    if ~fa.directory
-        error('%s is not a directory!', thisDir)
-    end
     if contains(rw, 'r') && ~fa.UserRead
         error('%s is not readable!', thisDir)
     end
     if contains(rw, 'w') && ~fa.UserWrite
         error('%s is not writeable!', thisDir)
+    end
+    if ~fa.directory
+        error('%s is not a directory!', thisDir)
     end
 
     % Finish up
