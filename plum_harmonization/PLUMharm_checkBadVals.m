2f01b0055ee73d9e335e1e88439430dd51cbe79f
Sam Rabin <samrabin@ucar.edu>
2025-07-08 18:20:52 -0600

    PLUMharm: Call PLUMharm_checkBadVals before PLUMharm_fixTinyNegs.

--- plum_harmonization/PLUMharm_checkBadVals.m
+++ plum_harmonization/PLUMharm_checkBadVals.m
@@ -11,15 +11,15 @@ if ~isempty(in_lu_YXv)
     in_luFrac_YXv = in_lu_YXv./repmat(landArea_YX, [1 1 size(in_lu_YXv,3)]) ;
     if any(isnan(in_lu_YXv(:)))
         error('NaN(s) in %s LU maps!', msg)
+    elseif any(isBare) && any(any(vegd_YX==0 & landArea_YX>0))
+        [i, j] = find(vegd_YX==0 & landArea_YX>0) ;
+        warning('Zero vegetation in cell(s) with land! Max land area %0.1e, total %0.1e. E.g.: i=%d, j=%d', max(landArea_YX(vegd_YX==0)), sum(landArea_YX(vegd_YX==0)), i(1), j(1))
     elseif min(in_luFrac_YXv(:))<0
         error('Negative value(s) in %s LU maps! Min %0.1e', msg, min(in_luFrac_YXv(:)))
     elseif max(in_luFrac_YXv(:))>1+10^-outPrec
         error('Value(s) >1 in %s LU maps! Max %0.1e', msg, max(in_luFrac_YXv(:))-1)
     elseif max(max(sum(in_luFrac_YXv,3)))>1+10^-outPrec
         error('Sum(s) >1 in %s LU maps! Max overage %0.1e', msg, max(max(sum(in_luFrac_YXv,3)))-1)
-    elseif any(isBare) && any(any(vegd_YX==0 & landArea_YX>0))
-        [i, j] = find(vegd_YX==0 & landArea_YX>0) ;
-        warning('Zero vegetation in cell(s) with land! Max land area %0.1e, total %0.1e. E.g.: i=%d, j=%d', max(landArea_YX(vegd_YX==0)), sum(landArea_YX(vegd_YX==0)), i(1), j(1))
     end
 end
 
