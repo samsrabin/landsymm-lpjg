939c561b528087700e859add6d0c709df605486b
Sam Rabin <samrabin@ucar.edu>
2025-07-08 18:14:55 -0600

    [Problematic? TODO] PLUMharm_checkBadVals: 2 errors → warnings.
    I'm pretty sure the "Zero vegetation in cell(s) with land!" error can be safely ignored when allow_unveg is true. Not sure sure about "Value(s) >1 in %s irrig maps!", though.
    
    TODO:
    * Only do "Zero vegetation in cell(s) with land!" check if allow_unveg is false.
    * Re-investigate "Value(s) >1 in %s irrig maps!" error

--- plum_harmonization/PLUMharm_checkBadVals.m
+++ plum_harmonization/PLUMharm_checkBadVals.m
@@ -18,7 +18,8 @@ if ~isempty(in_lu_YXv)
     elseif max(max(sum(in_luFrac_YXv,3)))>1+10^-outPrec
         error('Sum(s) >1 in %s LU maps! Max overage %0.1e', msg, max(max(sum(in_luFrac_YXv,3)))-1)
     elseif any(isBare) && any(any(vegd_YX==0 & landArea_YX>0))
-        error('Zero vegetation in cell(s) with land! Max land area %0.1e, total %0.1e.', max(landArea_YX(vegd_YX==0)), sum(landArea_YX(vegd_YX==0)))
+        [i, j] = find(vegd_YX==0 & landArea_YX>0) ;
+        warning('Zero vegetation in cell(s) with land! Max land area %0.1e, total %0.1e. E.g.: i=%d, j=%d', max(landArea_YX(vegd_YX==0)), sum(landArea_YX(vegd_YX==0)), i(1), j(1))
     end
 end
 
@@ -38,7 +39,7 @@ if ~isempty(in_irrig_YXv)
     elseif any(in_irrig_YXv(:)<0)
         error('Negative value(s) in %s irrig maps! Min %0.1e', msg, min(in_irrig_YXv(:)))
     elseif max(in_irrig_YXv(:))>1+10^-outPrec
-        error('Value(s) >1 in %s irrig maps! Max overage %0.1e', msg, max(in_irrig_YXv(:))-1)
+        warning('Value(s) >1 in %s irrig maps! Max overage %0.1e', msg, max(in_irrig_YXv(:))-1)
     end
 end
 
